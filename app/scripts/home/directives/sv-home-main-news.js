6c061ec53a48f40b35e7f0c027512a0eecb885e0
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-25 15:13:36 -0500

    Add files

---
+++ app/scripts/home/directives/sv-home-main-news.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    angular.module('home')
+        .directive('svHomeMainNews', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/home/directives/sv-home-main-news.html',
+                scope: {},
+                bindToController: {
+                    someObject: '=',
+                    someString: '@',
+                    someExpr: '&'
+                },
+                controllerAs: 'ctrl',
+                controller: function ($scope) {
+                    var ctrl = this;
+
+                },
+
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
