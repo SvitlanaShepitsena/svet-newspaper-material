3cdfb8a0d65534a2603209dc22df184bee1311ab
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-25 13:06:05 -0500

    Icons

---
+++ app/scripts/settings/icons.js
@@ -0,0 +1,14 @@
+(function () {
+    angular.module('app')
+        .config(function ($mdIconProvider) {
+            $mdIconProvider
+                .defaultIconSet("./assets/svg/avatars.svg", 128)
+                .icon("menu", "./assets/svg/menu.svg", 24)
+                .icon("share", "./assets/svg/share.svg", 24)
+                .icon("google_plus", "./assets/svg/google_plus.svg", 512)
+                .icon("hangouts", "./assets/svg/hangouts.svg", 512)
+                .icon("twitter", "./assets/svg/twitter.svg", 512)
+                .icon("phone", "./assets/svg/phone.svg", 512);
+    });
+})();
+
--- gulp/watch.js
+++ gulp/watch.js
@@ -10,7 +10,6 @@ gulp.task('watch', ['wiredep'], function () {
     gulp.watch('app/styles/**/*.styl', ['stylus']);
     gulp.watch('app/scripts/**/*.styl', ['stylus']);
     gulp.watch('app/**/*.jade', ['jade']);
-    gulp.watch('app/scripts/**/*.js', ['scripts']);
     //gulp.watch('app/images/**/*', ['images']);
     //gulp.watch('app/**/*', ['add']);
 
