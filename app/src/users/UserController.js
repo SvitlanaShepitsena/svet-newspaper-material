e609727f91d23a2d5423158d7f27cca1918cd9cf
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-25 09:45:33 -0500

    mdi installed

--- app/src/users/UserController.js
+++ app/src/users/UserController.js
@@ -1,96 +1,35 @@
-(function(){
+(function () {
 
-  angular
-       .module('users')
-       .controller('UserController', [
-          'userService', '$mdSidenav', '$mdBottomSheet', '$log', '$q',
-          UserController
-       ]);
-
-  /**
-   * Main Controller for the Angular Material Starter App
-   * @param $scope
-   * @param $mdSidenav
-   * @param avatarsService
-   * @constructor
-   */
-  function UserController( userService, $mdSidenav, $mdBottomSheet, $log, $q) {
-    var self = this;
-
-    self.selected     = null;
-    self.users        = [ ];
-    self.selectUser   = selectUser;
-    self.toggleList   = toggleUsersList;
-    self.share        = share;
-
-    // Load all registered users
-
-    userService
-          .loadAllUsers()
-          .then( function( users ) {
-            self.users    = [].concat(users);
-            self.selected = users[0];
-          });
-
-    // *********************************
-    // Internal methods
-    // *********************************
-
-    /**
-     * First hide the bottomsheet IF visible, then
-     * hide or Show the 'left' sideNav area
-     */
-    function toggleUsersList() {
-      var pending = $mdBottomSheet.hide() || $q.when(true);
-
-      pending.then(function(){
-        $mdSidenav('left').toggle();
-      });
-    }
-
-    /**
-     * Select the current avatars
-     * @param menuId
-     */
-    function selectUser ( user ) {
-      self.selected = angular.isNumber(user) ? $scope.users[user] : user;
-      self.toggleList();
-    }
-
-    /**
-     * Show the bottom sheet
-     */
-    function share($event) {
-        var user = self.selected;
-
-        $mdBottomSheet.show({
-          parent: angular.element(document.getElementById('content')),
-          templateUrl: '/src/users/view/contactSheet.html',
-          controller: [ '$mdBottomSheet', UserSheetController],
-          controllerAs: "vm",
-          bindToController : true,
-          targetEvent: $event
-        }).then(function(clickedItem) {
-          clickedItem && $log.debug( clickedItem.name + ' clicked!');
+    angular.module('users', ['ngMaterial'])
+        .controller('UserController', function ($scope, $timeout, $mdSidenav, $log) {
+            $scope.toggleLeft = function () {
+                $mdSidenav('left').toggle()
+                    .then(function () {
+                        $log.debug("toggle left is done");
+                    });
+            };
+            $scope.toggleRight = function () {
+                $mdSidenav('right').toggle()
+                    .then(function () {
+                        $log.debug("toggle RIGHT is done");
+                    });
+            };
+        })
+        .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+            $scope.close = function () {
+                $mdSidenav('left').close()
+                    .then(function () {
+                        $log.debug("close LEFT is done");
+                    });
+            };
+        })
+        .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+            $scope.close = function () {
+                $mdSidenav('right').close()
+                    .then(function () {
+                        $log.debug("close RIGHT is done");
+                    });
+            };
         });
-
-        /**
-         * Bottom Sheet controller for the Avatar Actions
-         */
-        function UserSheetController( $mdBottomSheet ) {
-          this.user = user;
-          this.items = [
-            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
-          ];
-          this.performAction = function(action) {
-            $mdBottomSheet.hide(action);
-          };
-        }
-    }
-
-  }
 
 })();
--- app/src/users/Users.js
+++ app/src/users/Users.js
@@ -2,7 +2,14 @@
   'use strict';
 
   // Prepare the 'users' module for subsequent registration of controllers and delegates
-  angular.module('users', [ 'ngMaterial' ]);
+  angular.module('users', [ 'ngMaterial' ])
+      .config(function () {
+      console.log('config')
+  })
+      .run(function () {
+      console.log('run')
+  })
+  ;
 
 
 })();
