43b83c7032dfe209e1f52951c8e8bd6e2126e7f6
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-26 12:51:03 -0500

    Add files

--- app/scripts/home/directives/sv-home-main-news.js
+++ app/scripts/home/directives/sv-home-main-news.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('home')
-        .directive('svHomeMainNews', function () {
+        .directive('svHomeMainNews', function (ImageSizeServ) {
             return {
                 replace: true,
                 templateUrl: 'scripts/home/directives/sv-home-main-news.html',
@@ -18,7 +18,19 @@
                 },
 
                 link: function ($scope, el, attrs) {
-
+                    $scope.imageurl = '/img/news/3-25-2015/gorlovka.jpg';
+                    $scope.image = {
+                        path: "",
+                        width: 0,
+                        height: 0
+                    }
+                    var img = new Image();
+                    img.onload = function () {
+                        $scope.image.width = img.width;
+                        $scope.image.height = img.height;
+                        $scope.image.path = '/img/news/3-25-2015/gorlovka.jpg';
+                    }
+                    img.src = $scope.imageurl;
                 }
             };
         });
