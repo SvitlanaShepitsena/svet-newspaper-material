d1a0501a564d2daa5f2dcfd20fa2c9e1b6b4c163
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-26 15:12:52 -0500

    Add files

---
+++ app/scripts/home/directives/sv-svet-social.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    angular.module('home')
+        .directive('svSvetSocial', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/home/directives/sv-svet-social.html',
+                scope: {},
+                bindToController: {
+                    someObject: '=',
+                    someString: '@',
+                    someExpr: '&'
+                },
+                controllerAs: 'ctrl',
+                controller: function ($scope) {
+                    var ctrl = this;
+
+                },
+
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
