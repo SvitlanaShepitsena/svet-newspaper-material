0469df8de2c04ab204ed718f0f96deec099f4cae
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-27 13:31:39 -0500

    Responsive Log Fab buttons

--- app/scripts/auth/directives/sv-auth-btn.js
+++ app/scripts/auth/directives/sv-auth-btn.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('auth')
-        .directive('svAuthBtn', function (AuthServ, $rootScope) {
+        .directive('svAuthBtn', function (AuthServ, $rootScope,$mdMedia) {
             return {
                 templateUrl: 'scripts/auth/directives/sv-auth-btn.html',
                 replace: true,
@@ -11,6 +11,15 @@
                 controllerAs: 'ctrl',
                 controller: function ($scope) {
                     var ctrl = this;
+
+                    $scope.$watch(function() { return $mdMedia('gt-sm'); }, function(size) {
+                        ctrl.gtSm = size;
+                    });
+                    $scope.$watch(function() { return $mdMedia('sm'); }, function(size) {
+
+                        ctrl.sm = size;
+                    });
+
 
                     $rootScope.$watch('user', function (newVal, oldVal) {
                         $scope.user = newVal;
