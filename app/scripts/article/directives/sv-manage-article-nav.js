5f3ef44b2faac6087b2731325f5da3061f7abdea
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-29 14:02:14 -0500

    Add files

---
+++ app/scripts/article/directives/sv-manage-article-nav.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    angular.module('article')
+        .directive('svManageArticleNav', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/article/directives/sv-manage-article-nav.html',
+                scope: {},
+                bindToController: {
+
+                },
+                controllerAs: 'ctrl',
+                controller: function ($scope) {
+                    var ctrl = this;
+
+                },
+
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
