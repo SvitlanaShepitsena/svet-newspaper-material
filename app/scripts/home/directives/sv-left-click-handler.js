263635d3f4a443a431979a583885648f01d20e6d
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-29 14:30:26 -0500

    Add files

--- app/scripts/home/directives/sv-left-click-handler.js
+++ app/scripts/home/directives/sv-left-click-handler.js
@@ -4,19 +4,14 @@
     angular.module('home')
         .directive('svLeftClickHandler', function () {
             return {
-                replace: true,
-                templateUrl: 'scripts/home/directives/sv-left-click-handler.html',
-                scope: {},
-                bindToController: {
-
-                },
-                controllerAs: 'ctrl',
-                controller: function ($scope) {
-                    var ctrl = this;
-
-                },
-
                 link: function ($scope, el, attrs) {
+                    el.on('touchstart', function (e) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        $scope.$apply(function () {
+                            $scope.toggleLeft();
+                        });
+                    })
 
                 }
             };
