0353e41c0c9b3370022c1e7356302164d0d78e5f
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-31 16:06:56 -0500

    Main news Pics

--- app/scripts/article/directives/sv-top-news.js
+++ app/scripts/article/directives/sv-top-news.js
@@ -8,12 +8,19 @@
                 templateUrl: 'scripts/article/directives/sv-top-news.html',
                 scope: {},
                 bindToController: {
-                    mainNews:'='
+                    mainNews:'=',
+                    isFirst:'='
                 },
                 controllerAs: 'ctrl',
                 controller: function ($scope) {
+
                     var ctrl = this;
+
+                    if (ctrl.isFirst) {
                     ctrl.image = faker.image.city(400,238);
+                    } else{
+                    ctrl.image = faker.image.animals(400,238);
+                    }
 
                 },
 
