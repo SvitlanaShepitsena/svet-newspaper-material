99a8da8e83358f1bd15d4825dc2492707efb8519
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-31 16:23:26 -0500

    Sections for Classified With Dynamic Angular Material Tabs

--- app/scripts/classified/controllers/ClassifiedCtrl.js
+++ app/scripts/classified/controllers/ClassifiedCtrl.js
@@ -2,41 +2,16 @@
     'use strict';
 
     angular.module('classified')
-        .controller('ClassifiedCtrl', function ($scope) {
+        .controller('ClassifiedCtrl', function ($scope, $log) {
             var classified = this;
-
             var tabs = [
-                    {title: 'One', content: "Tabs will become paginated if there isn't enough room for them."},
-                    {title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
-                    {
-                        title: 'Three',
-                        content: "You can bind the selected tab via the selected attribute on the md-tabs element."
-                    },
-                    {
-                        title: 'Four',
-                        content: "If you set the selected tab binding to -1, it will leave no tab selected."
-                    },
-                    {title: 'Five', content: "If you remove a tab, it will try to select a new one."},
-                    {
-                        title: 'Six',
-                        content: "There's an ink bar that follows the selected tab, you can turn it off if you want."
-                    },
-                    {
-                        title: 'Seven',
-                        content: "If you set ng-disabled on a tab, it becomes unselectable. If the currently selected tab becomes disabled, it will try to select the next tab."
-                    },
-                    {
-                        title: 'Eight',
-                        content: "If you look at the source, you're using tabs to look at a demo for tabs. Recursion!"
-                    },
-                    {
-                        title: 'Nine',
-                        content: "If you set md-theme=\"green\" on the md-tabs element, you'll get green tabs."
-                    },
-                    {
-                        title: 'Ten',
-                        content: "If you're still reading this, you should just go check out the API docs for tabs!"
-                    }
+                    {title: 'Община', content: "Here are classified for community"},
+                    {title: 'Продается', content: "Here are classified for sale."},
+                    {title: 'Работа', content: "Here are classified for jobs"},
+                    {title: 'Услуги', content: "Here are classified for services"},
+                    {title: 'Недвижимость', content: "Here are classified for housing"},
+                    {title: 'Авто', content: "Here are classified for cars"},
+                    {title: 'Знакомства', content: "Here are classified for personal"}
                 ],
                 selected = null,
                 previous = null;
@@ -48,16 +23,6 @@
                 if (old && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
                 if (current)                $log.debug('Hello ' + selected.title + '!');
             });
-            $scope.addTab = function (title, view) {
-                view = view || title + " Content View";
-                tabs.push({title: title, content: view, disabled: false});
-            };
-            $scope.removeTab = function (tab) {
-                var index = tabs.indexOf(tab);
-                tabs.splice(index, 1);
-            };
-
-
         });
 })();
 
