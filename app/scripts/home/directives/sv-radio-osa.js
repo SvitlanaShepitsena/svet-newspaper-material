07fd7913529af3bfce0db95f66656222e0842d38
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-01 14:39:15 -0500

    Add files

---
+++ app/scripts/home/directives/sv-radio-osa.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    angular.module('home')
+        .directive('svRadioOsa', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/home/directives/sv-radio-osa.html',
+                scope: {},
+                bindToController: {
+
+                },
+                controllerAs: 'ctrl',
+                controller: function ($scope) {
+                    var ctrl = this;
+
+                },
+
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
