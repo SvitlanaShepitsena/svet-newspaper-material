6433327ce79e23e3ff05b60afc95648f25417481
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-01 15:04:40 -0500

    Add files

---
+++ app/scripts/article/services/NewsProcessServ.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('article')
+        .factory('NewsProcessServ', function ($q, url) {
+            return {
+                getSync: function () {
+
+                },
+                get: function () {
+                    return $q(function (resolve, reject) {
+
+                    });
+                }
+            };
+        });
+})();
