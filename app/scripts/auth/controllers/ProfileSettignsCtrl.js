581f915986844782197ded2ace2bb6752f4aa68f
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-31 21:47:44 -0500

    Add files

---
+++ app/scripts/auth/controllers/ProfileSettignsCtrl.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular.module('auth')
+        .controller('ProfileSettignsCtrl', function ($scope) {
+            var profileSettigns = this;
+
+        });
+})();
+
