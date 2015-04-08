98cbabcdbbae71881b0c3cd79073951cc643830a
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-08 14:00:43 -0500

    Add files

---
+++ app/scripts/events/directives/sv-event-sidenav-ad.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    angular.module('events')
+        .directive('svEventSidenavAd', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/events/directives/sv-event-sidenav-ad.html',
+                scope: {},
+                bindToController: {
+
+                },
+                controllerAs: 'ctrl',
+                controller: function ($scope) {
+                    var ctrl = this;
+
+                },
+
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
