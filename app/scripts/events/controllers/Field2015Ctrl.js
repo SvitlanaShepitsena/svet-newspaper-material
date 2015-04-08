ad023e33867d629c3b5ba9cb28bb6e99053db219
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-08 14:33:40 -0500

    Add files

---
+++ app/scripts/events/controllers/Field2015Ctrl.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular.module('events')
+        .controller('Field2015Ctrl', function ($scope) {
+            var field2015 = this;
+
+        });
+})();
+
