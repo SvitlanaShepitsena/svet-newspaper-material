7303bab99cadc54917331d64b1f26b48e3e525d3
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-09 15:23:27 -0500

    Abstract view for events

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -113,26 +113,27 @@
                     templateUrl: "scripts/events/views/eventsCtrl.html"
                 })
 
-                .state("app.field-event", {
+                .state("app.events.calendar", {
+                    url: "/event-calendar",
+                    controller:"EventCalendarCtrl as eventCalendar",
+                    templateUrl: "scripts/events/views/event-calendarCtrl.html"
+                })
+
+                .state("app.events.field", {
                     url: "/events/field-event/:year",
                     controller: "FieldEventCtrl as fieldEvent",
                     templateUrl: "scripts/events/views/field-eventCtrl.html"
                 })
-                .state("app.ravinia-event", {
+                .state("app.events.ravinia", {
                     url: "/ravinia-event",
                     controller: "RaviniaEventCtrl as raviniaEvent",
                     templateUrl: "scripts/events/views/ravinia-eventCtrl.html"
                 })
 
-				.state("app.events-photo-gallery", {
+				.state("app.events.photo-gallery", {
 					url: "/events-photo-gallery",
 					controller:"EventsPhotoGalleryCtrl as eventsPhotoGallery",
 					templateUrl: "scripts/events/views/events-photo-galleryCtrl.html"
-				})
-				.state("app.event-calendar", {
-					url: "/event-calendar", 
-					controller:"EventCalendarCtrl as eventCalendar",
-					templateUrl: "scripts/events/views/event-calendarCtrl.html"
 				})
 //#state'
         });
