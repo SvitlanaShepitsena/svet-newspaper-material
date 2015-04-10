ea30eb3bb1444a48fd30a2888e168769d1cf649a
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-10 09:50:08 -0500

    Please Preprocessor

--- gulp/watch.js
+++ gulp/watch.js
@@ -2,14 +2,31 @@
 
 var gulp = require('gulp');
 var shell = require('gulp-shell')
+var please = require('gulp-pleeease');
+var rename = require("gulp-rename");
+var plumber = require('gulp-plumber');
+
 gulp.task('add', shell.task([
     'git add .',
 ]))
+
+gulp.task('please', function () {
+    gulp.src(['./app/styles/main.css','./app/assets/app.css'])
+        .pipe(plumber())
+        .pipe(please())
+        .pipe(rename({
+            suffix: '.min',
+            extname: '.css'
+        }))
+        .pipe(gulp.dest('./app/styles/'));
+});
 
 gulp.task('watch', function () {
     gulp.watch('app/styles/**/*.styl', ['stylus']);
     gulp.watch('app/scripts/**/*.styl', ['stylus']);
     gulp.watch('app/**/*.jade', ['jade']);
+    gulp.watch('app/styles/main.css', ['please']);
+
     //gulp.watch('app/images/**/*', ['images']);
     //gulp.watch('app/**/*', ['add']);
 
