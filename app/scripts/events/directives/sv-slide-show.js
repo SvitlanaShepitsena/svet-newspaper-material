5dcdf35e05abb7fab51e555f56112c6e0753a761
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-10 14:32:23 -0500

    Please Preprocessor

--- app/scripts/events/directives/sv-slide-show.js
+++ app/scripts/events/directives/sv-slide-show.js
@@ -8,11 +8,23 @@
                 templateUrl: 'scripts/events/directives/sv-slide-show.html',
                 scope: {},
                 bindToController: {
+                    numb:'=',
+                    path:'@',
+                    type:'@'
 
                 },
                 controllerAs: 'ctrl',
                 controller: function ($scope) {
                     var ctrl = this;
+                    console.log(ctrl.type);
+                    ctrl.slides = [];
+                    for (var i = 1; i <= ctrl.numb; i++) {
+                        var slide = {
+                            url:ctrl.path+'/'+i+'.'+ctrl.type
+                        }
+                        ctrl.slides.push(slide);
+
+                    }
 
                 },
 
