57dcd9e3cc96278957d074b286dcdcab531808b1
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-10 21:09:55 -0500

    ALL WORKSSS!!!!!

--- app/scripts/article/controllers/ArticleCtrl.js
+++ app/scripts/article/controllers/ArticleCtrl.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('article')
-        .controller('ArticleCtrl', function ($scope, $rootScope, $stateParams, newsRef) {
+        .controller('ArticleCtrl', function ($scope, $rootScope, $stateParams) {
             var article = this;
             var id = parseInt($stateParams.id);
 
