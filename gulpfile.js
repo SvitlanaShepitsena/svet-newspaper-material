2193d560f892825e68665f29ffa2ab41f6306411
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-13 15:22:22 -0500

    Gulpfile css vendors

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,91 @@
 'use strict';
 var gulp = require('gulp');
+var p = require('gulp-load-plugins')();
+var browserSync = require("browser-sync");
+var reload = browserSync.reload;
+var nib = require('nib');
+
+///////////////////////////// Error Handling ////////////////////////////////////////////////
+var onError = function (err) {
+    p.notify.onError({
+        title: "Gulp",
+        subtitle: "Failure!",
+        message: "Error: <%= error.message %>",
+        sound: "Beep"
+    })(err);
+
+    this.emit('end');
+};
+
+///////////////////////////// JADE ////////////////////////////////////////////////
+gulp.task("jade", function () {
+    return gulp.src(["./app/scripts/**/*.jade"])
+        .pipe(p.plumber({errorHandler: onError}))
+        .pipe(p.jade())
+        .pipe(gulp.dest("./app/scripts/"))
+});
+
+gulp.task('jade:watch', ['jade'], reload);
 
 
+///////////////////////////// Stylus/Css ////////////////////////////////////////////////
 
-/**********************
- ANGULAR
- ***********************/
-require('./gulp/ng/value.js');
-require('./gulp/ng/css-transitions.js');
-require('./gulp/ng/css-animations.js');
-require('./gulp/ng/js-animations.js');
+gulp.task("stylus:scripts", function () {
+    return gulp.src(["./app/scripts/**/*.styl",])
+        .pipe(p.plumber({errorHandler: onError}))
+        .pipe(p.stylus({use: nib()}))
+        .pipe(gulp.dest("./app/scripts/"))
+        .pipe(reload({stream: true}));
+
+    ;
+});
+
+gulp.task("stylus:main", ['stylus:scripts'], function () {
+    return gulp.src(["./app/styles/**/*.styl",])
+        .pipe(p.plumber({errorHandler: onError}))
+        .pipe(p.stylus({use: nib()}))
+        .pipe(gulp.dest("./app/styles/"))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task("assets:css", function () {
+    return gulp.src(["./app/index.html"])
+        .pipe(p.assets({
+            js: false,
+            css: true
+        }))
+        .pipe(p.autoprefixer())
+        .pipe(p.concat('vendors.css'))
+        .pipe(p.minifyCss())
+       .pipe(gulp.dest("./app/assets/"))
+});
+
+
+///////////////////////////// SERVER  ////////////////////////////////////////////////
+
+gulp.task('serve', ['stylus:main', 'jade'], function () {
+
+    browserSync({
+        server: "./app",
+        logLevel: "silent",
+        notify: false
+    });
+
+    gulp.watch(["./app/scripts/**/*.styl", "./app/styles/**/*.styl"], ['stylus:main']);
+
+    gulp.watch(['./app/scripts/**/*.jade'], ['jade:watch']);
+
+    gulp.watch("app/*.html").on('change', reload);
+});
+
+
+gulp.task('default', ['serve']);
+
+
+///**********************
+// ANGULAR
+// ***********************/
+//require('./gulp/ng/value.js');
+//require('./gulp/ng/css-transitions.js');
+//require('./gulp/ng/css-animations.js');
+//require('./gulp/ng/js-animations.js');
