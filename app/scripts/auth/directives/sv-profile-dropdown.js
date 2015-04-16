0f9e733546ae228a26609366191f9d4b4c5453bb
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-16 14:48:54 -0500

    Before video tutorial

--- app/scripts/auth/directives/sv-profile-dropdown.js
+++ app/scripts/auth/directives/sv-profile-dropdown.js
@@ -4,11 +4,7 @@
     angular.module('auth')
         .directive('svProfileDropdown', function () {
             return {
-                replace: true,
                 templateUrl: 'scripts/auth/directives/sv-profile-dropdown.html',
-                scope: {
-
-                },
                 link: function ($scope, el, attrs) {
 
                 }
