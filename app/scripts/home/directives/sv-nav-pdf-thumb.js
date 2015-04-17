ea27257a643cc17d4423218795ea0eb98f786856
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-17 16:18:31 -0500

    Pdf newspaper thumbnail design for home page #151; Contact Us button with custom design

--- app/scripts/home/directives/sv-nav-pdf-thumb.js
+++ app/scripts/home/directives/sv-nav-pdf-thumb.js
@@ -7,7 +7,8 @@
                 replace: true,
                 templateUrl: 'scripts/home/directives/sv-nav-pdf-thumb.html',
                 scope: {
-
+                    url: '@',
+                    img: '@'
                 },
                 link: function ($scope, el, attrs) {
 
