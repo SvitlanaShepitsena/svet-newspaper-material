ff249450e0efd7f056ca4e4333f5f804c55c6369
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-17 11:37:07 -0500

    Sepia Color Theme for Stylus

--- app/scripts/settings/colors.js
+++ app/scripts/settings/colors.js
@@ -1,29 +1,29 @@
 (function () {
     angular.module('app').config(function ($mdThemingProvider) {
         $mdThemingProvider.definePalette('newspaper', {
-            '50': 'F5E9DA',
+            '50': 'FAF7F2',
             '100': 'F0EBE1',
-            '200': 'C4B3A7',
+            '200': 'C4BEB5',
             '300': 'F0E9E1',
-            '400': '9C8778',
+            '400': 'C4BAAB',
             '500': '8C7769',
             '600': '7A685C',
             '700': '63554B',
             '800': '4F433B',
             '900': '38302A',
-            'A100': 'FAF7F2',
+            'A100': 'FAFAF5',
             'A200': 'F5E9DA',
             'A400': 'F5E9DA',
             'A700': 'F5E9DA',
             'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
                                                 // on this palette should be dark or light
-            'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-                '200', '300', '400', 'A100'],
             'contrastLightColors': undefined    // could also specify this if default was 'dark'
         });
         $mdThemingProvider.theme('default')
             .primaryPalette('newspaper', {
-                'default': '300'
+                'default': '400',
+                'hue-1': '100',
+                'hue-2': '600'
             })
             .accentPalette('red', {
                 'default': '600',
