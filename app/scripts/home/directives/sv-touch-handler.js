e8408d14004184431f8387a2f84ac75f3ecc1aa5
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-21 12:57:39 -0500

    Add files

--- app/scripts/home/directives/sv-touch-handler.js
+++ app/scripts/home/directives/sv-touch-handler.js
@@ -4,12 +4,11 @@
     angular.module('home')
         .directive('svTouchHandler', function () {
             return {
-                replace: true,
-                templateUrl: 'scripts/home/directives/sv-touch-handler.html',
-                scope: {
-
-                },
                 link: function ($scope, el, attrs) {
+                    el.on('touchmove', function (evt) {
+                        console.log(evt);
+                        evt.stopPropagation();
+                    });
 
                 }
             };
