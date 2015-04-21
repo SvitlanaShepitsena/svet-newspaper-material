fbca6a07697d1a7eeef2daddcdd139adda589dfc
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-21 15:54:46 -0500

    Build

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -61,10 +61,9 @@
                 var $rootScope = $injector.get('$rootScope');
                 var toastr = $injector.get('toastr');
                 exception.message += ' (caused by "' + cause + '")';
-                toastr.error('ERROR!'+exception.message);
-                $rootScope.$broadcast('error');
-
-                throw exception;
+                //toastr.error('ERROR!'+exception.message);
+                //$rootScope.$broadcast('error');
+                //throw exception;
             };
         }).config(['$compileProvider', function ($compileProvider) {
         $compileProvider.debugInfoEnabled(false);
