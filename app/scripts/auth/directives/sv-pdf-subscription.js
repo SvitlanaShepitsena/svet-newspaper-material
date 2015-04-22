4d0cdefbd6fe2f1d5e74cc3f354c18ba459f712f
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-22 12:45:01 -0500

    User Dashboard Tabs Content with templates

--- app/scripts/auth/directives/sv-pdf-subscription.js
+++ app/scripts/auth/directives/sv-pdf-subscription.js
@@ -2,14 +2,19 @@
     'use strict';
 
     angular.module('auth')
-        .directive('svPdfSubscription', function () {
+        .directive('svPdfSubscription', function (PdfSubscriptionsServ) {
             return {
                 replace: true,
                 templateUrl: 'scripts/auth/directives/sv-pdf-subscription.html',
                 scope: {
 
                 },
-                link: function ($scope, el, attrs) {
+                link: function ($scope, user) {
+                    var pdfSubObj = PdfSubscriptionsServ.getObjectRef(user.id);
+
+                    pdfSubObj.$bindTo($scope, 'pdfSub').then(function () {
+                        $scope.pdfLoaded = true;
+                    });
 
                 }
             };
