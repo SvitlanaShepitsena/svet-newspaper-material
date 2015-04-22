12b7346803d11b4697b2e42eefcd0b8ca93a2704
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-22 16:15:55 -0500

    git bug

--- app/scripts/settings/icons.js
+++ app/scripts/settings/icons.js
@@ -3,7 +3,7 @@
         .config(function ($mdIconProvider) {
             $mdIconProvider
                 .defaultIconSet("./assets/svg/avatars.svg", 128)
-                .icon("menu", "./assets/svg/menu.svg", 24)
+                .icon("sv-menu", "./assets/svg/menu.svg", 24)
                 .icon("share", "./assets/svg/share.svg", 24)
                 .icon("google_plus", "./assets/svg/google_plus.svg", 512)
                 .icon("hangouts", "./assets/svg/hangouts.svg", 512)
