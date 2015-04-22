a8133326bac0a7678a2fdf726c230e9bfed978ec
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-21 23:35:32 -0500

    Directive for User Social Community  Profile Section Navigation (Angular Material Tabs)

--- app/scripts/auth/directives/sv-user-social-tabs.js
+++ app/scripts/auth/directives/sv-user-social-tabs.js
@@ -6,10 +6,20 @@
             return {
                 replace: true,
                 templateUrl: 'scripts/auth/directives/sv-user-social-tabs.html',
-                scope: {
-
-                },
+                scope: {},
                 link: function ($scope, el, attrs) {
+                    var tabs = [
+                            {title: 'friends', content: "My Friends "},
+                            {title: 'community', content: "Find People"}
+                        ],
+                        selected = null,
+                        previous = null;
+                    $scope.tabs = tabs;
+                    $scope.selectedIndex = 0;
+                    $scope.$watch('selectedIndex', function (current, old) {
+                        previous = selected;
+                        selected = tabs[current];
+                    });
 
                 }
             };
