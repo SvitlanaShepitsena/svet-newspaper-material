0bcdbcc452204b9fbebf42cdb2b70c14750038af
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-23 11:36:56 -0500

    Add files

---
+++ app/scripts/home/services/NewsServ.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('home')
+        .factory('NewsServ', function ($q, url) {
+            return {
+                getSync: function () {
+
+                },
+                get: function () {
+                    return $q(function (resolve, reject) {
+
+                    });
+                }
+            };
+        });
+})();
