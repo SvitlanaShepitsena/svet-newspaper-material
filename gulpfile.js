7bd72ad1867df2e9382fdd9a8868f6c8929fd329
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-23 10:13:26 -0500

    Toolbar change

--- gulpfile.js
+++ gulpfile.js
@@ -4,96 +4,92 @@ var p = require('gulp-load-plugins')();
 
 var browserSync = require("browser-sync");
 var reload = browserSync.reload;
-var cmq = require('gulp-combine-media-queries');
+var cmq = require('gulp-combine-media-queries');
 
 var nib = require('nib');
 
 ///////////////////////////// Error Handling ////////////////////////////////////////////////
 var onError = function (err) {
-    p.notify.onError({
-        title: "Gulp",
-        subtitle: "Failure!",
-        message: "Error: <%= error.message %>",
-        sound: "Beep"
-    })(err);
+	p.notify.onError({
+		title: "Gulp",
+		subtitle: "Failure!",
+		message: "Error: <%= error.message %>",
+		sound: "Beep"
+	})(err);
 
-    this.emit('end');
+	this.emit('end');
 };
 
 ///////////////////////////// JADE ////////////////////////////////////////////////
 gulp.task("jade", function () {
-    return gulp.src(["./app/scripts/**/*.jade"])
-        .pipe(p.plumber({errorHandler: onError}))
-        .pipe(p.jade())
-        .pipe(gulp.dest("./app/scripts/"))
+	return gulp.src(["./app/scripts/**/*.jade"])
+		.pipe(p.plumber({errorHandler: onError}))
+		.pipe(p.jade())
+		.pipe(gulp.dest("./app/scripts/"))
 });
 
 gulp.task('jade:watch', ['jade'], browserSync.reload);
 
-
 ///////////////////////////// Stylus/Css ////////////////////////////////////////////////
 
-gulp.task("stylus:scripts", function () {
-    return gulp.src(["./app/scripts/**/*.styl",])
-        .pipe(p.plumber({errorHandler: onError}))
-        .pipe(p.stylus({use: nib()}))
-        .pipe(gulp.dest("./app/scripts/"))
-        .pipe(reload({stream: true}));
 
-    ;
+gulp.task("stylus:scripts", function () {
+	return gulp.src(["./app/scripts/**/*.styl",])
+		.pipe(p.plumber({errorHandler: onError}))
+		.pipe(p.stylus({use: nib()}))
+		.pipe(gulp.dest("./app/scripts/"))
+		.pipe(reload({stream: true}));
+
+	;
 });
 
 gulp.task("stylus:main", ['stylus:scripts'], function () {
-    return gulp.src(["./app/styles/**/*.styl",])
-        .pipe(p.plumber({errorHandler: onError}))
-        .pipe(p.stylus({use: nib()}))
-        .pipe(p.filter('*.css'))
-        .pipe(p.pleeease({
-            minifier: false,
-            "browsers": ["ie 10"]
-        }))
-        .pipe(cmq({
-            log: true
-        }))
-        .pipe(gulp.dest("./app/styles/"))
-        .pipe(reload({stream: true}));
+	return gulp.src(["./app/styles/**/*.styl",])
+		.pipe(p.plumber({errorHandler: onError}))
+		.pipe(p.stylus({use: nib()}))
+		.pipe(p.filter('*.css'))
+		.pipe(p.pleeease({
+			minifier: false,
+			"browsers": ["ie 10"]
+		}))
+		.pipe(cmq({
+			log: true
+		}))
+		.pipe(gulp.dest("./app/styles/"))
+		.pipe(reload({stream: true}));
 });
 
 gulp.task('media', function () {
-    gulp.src('./app/styles/main.css')
-        .pipe(cmq({
-            log: true
-        }))
-        .pipe(gulp.dest('dist'));
+	gulp.src('./app/styles/main.css')
+		.pipe(cmq({
+			log: true
+		}))
+		.pipe(gulp.dest('dist'));
 });
 ///////////////////////////// JS  ////////////////////////////////////////////////
 gulp.task('js-watch', browserSync.reload);
-
 
 ///////////////////////////// SERVER  ////////////////////////////////////////////////
 
 gulp.task('serve', ['stylus:main', 'jade'], function () {
 
-    browserSync({
-        server: "./app",
-        logLevel: "silent",
-        notify: false
-    });
+	browserSync({
+		server: "./app",
+		logLevel: "silent",
+		notify: false
+	});
 
-    gulp.watch(["./app/scripts/**/*.styl", "./app/styles/**/*.styl"], ['stylus:main']);
+	gulp.watch(["./app/scripts/**/*.styl", "./app/styles/**/*.styl"], ['stylus:main']);
 
-    gulp.watch(['./app/scripts/**/*.jade'], ['jade:watch']);
+	gulp.watch(['./app/scripts/**/*.jade'], ['jade:watch']);
 
-    gulp.watch("app/**/*.js", ['js-watch']);
+	gulp.watch("app/**/*.js", ['js-watch']);
 
-    gulp.watch("app/*.html").on('change', reload);
-
+	gulp.watch("app/*.html").on('change', reload);
 
 });
 
-
 gulp.task('default', ['serve']);
-
 
 ///**********************
 // ANGULAR
