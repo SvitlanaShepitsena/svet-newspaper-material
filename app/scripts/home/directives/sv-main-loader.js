8fdd556bd878bd19aaa656137f58bf474e8b7ffa
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-24 13:07:06 -0500

    App Loader for Home Page bugs fixed

--- app/scripts/home/directives/sv-main-loader.js
+++ app/scripts/home/directives/sv-main-loader.js
@@ -4,6 +4,7 @@
     angular.module('home')
         .directive('svMainLoader', function () {
             return {
+                replace: true,
                 templateUrl: 'scripts/home/directives/sv-main-loader.html'
             };
         });
