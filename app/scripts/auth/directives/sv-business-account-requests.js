e5655512bda04fbf120dfb6e2b23c2ae833ef383
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-24 16:51:14 -0500

    Manage Business Profile Request design (Manager Account) #194

--- app/scripts/auth/directives/sv-business-account-requests.js
+++ app/scripts/auth/directives/sv-business-account-requests.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('auth')
-        .directive('svBusinessAccountRequests', function () {
+        .directive('svBusinessAccountRequests', function (RequestServ, toastr) {
             return {
                 replace: true,
                 templateUrl: 'scripts/auth/directives/sv-business-account-requests.html',
@@ -10,6 +10,26 @@
 
                 },
                 link: function ($scope, el, attrs) {
+                    RequestServ.getAllRequests().then(function (requests) {
+                        $scope.users = requests;
+                    })
+
+                    $scope.accept = function (user) {
+
+                        RequestServ.acceptRequest(user.id).then(function () {
+                            var index = $scope.users.indexOf(user);
+                            $scope.users.splice(index,1);
+                            toastr.info('request has been accepted');
+                        })
+                    };
+                    $scope.reject = function (user) {
+                        RequestServ.cancelRequest(user.id).then(function () {
+                            var index = $scope.users.indexOf(user);
+                            $scope.users.splice(index,1);
+                            toastr.info('request has been rejected');
+                        })
+
+                    };
 
                 }
             };
--- app/scripts/auth/services/RequestServ.js
+++ app/scripts/auth/services/RequestServ.js
@@ -1,65 +1,106 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	angular.module('auth')
-		.factory('RequestServ', function ($q, users, $firebaseObject) {
-			return {
-				getStatus: function (userId) {
-					return $q(function (resolve, reject) {
+    angular.module('auth')
+        .factory('RequestServ', function ($q, users, $firebaseObject,$firebaseArray) {
+            return {
+                getStatus: function (userId) {
+                    return $q(function (resolve, reject) {
 
-						var userUrl = users + userId;
-						var userObj = $firebaseObject(new Firebase(userUrl));
+                        var userUrl = users + userId;
+                        var userObj = $firebaseObject(new Firebase(userUrl));
 
-						userObj.$loaded().then(function () {
-							if (userObj.requestCorporateSubmited) {
-								resolve(true);
-							} else {
-								resolve(false)
-							}
-						}).catch(function (error) {
-							console.log(userid + 'does not exists');
-							reject(error);
-						});
+                        userObj.$loaded().then(function () {
+                            if (userObj.requestCorporateSubmited) {
+                                resolve(true);
+                            } else {
+                                resolve(false)
+                            }
+                        }).catch(function (error) {
+                            console.log(userid + 'does not exists');
+                            reject(error);
+                        });
 
-					});
-				},
-				submitRequest: function (userId) {
-					return $q(function (resolve, reject) {
+                    });
+                },
+                submitRequest: function (userId) {
+                    return $q(function (resolve, reject) {
 
-						var userUrl = users + userId;
-						var userObj = $firebaseObject(new Firebase(userUrl));
+                        var userUrl = users + userId;
+                        var userObj = $firebaseObject(new Firebase(userUrl));
 
-						userObj.$loaded().then(function () {
-							userObj.requestCorporateSubmited = true;
+                        userObj.$loaded().then(function () {
+                            userObj.requestCorporateSubmited = true;
 
-							userObj.$save().then(function (success) {
-								resolve(success);
-							})
-						}).catch(function (error) {
-							console.log(userid + 'does not exists');
-							reject(error);
-						});
+                            userObj.$save().then(function (success) {
+                                resolve(success);
+                            })
+                        }).catch(function (error) {
+                            console.log(userid + 'does not exists');
+                            reject(error);
+                        });
 
-					});
-				},
-				cancelRequest: function (userId) {
-					return $q(function (resolve, reject) {
+                    });
+                },
+                cancelRequest: function (userId) {
+                    return $q(function (resolve, reject) {
 
-						var userUrl = users + userId;
-						var userObj = $firebaseObject(new Firebase(userUrl));
+                        var userUrl = users + userId;
+                        var userObj = $firebaseObject(new Firebase(userUrl));
 
-						userObj.$loaded().then(function () {
-							userObj.requestCorporateSubmited = false;
-							userObj.$save().then(function (success) {
-								resolve(success);
-							})
-						}).catch(function (error) {
-							console.log(userid + 'does not exists');
-							reject(error);
-						});
+                        userObj.$loaded().then(function () {
+                            userObj.requestCorporateSubmited = false;
+                            userObj.$save().then(function (success) {
+                                resolve(success);
+                            })
+                        }).catch(function (error) {
+                            console.log(userid + 'does not exists');
+                            reject(error);
+                        });
 
-					});
-				}
-			};
-		});
+                    });
+                },
+                acceptRequest: function (userId) {
+                    return $q(function (resolve, reject) {
+
+                        var userUrl = users + userId;
+                        var userObj = $firebaseObject(new Firebase(userUrl));
+
+                        userObj.$loaded().then(function () {
+                            userObj.requestCorporateSubmited = false;
+                            userObj.groups=[''];
+                            userObj.$save().then(function (success) {
+                                resolve(success);
+                            })
+                        }).catch(function (error) {
+                            console.log(userid + 'does not exists');
+                            reject(error);
+                        });
+
+                    });
+                },
+                getAllRequests: function () {
+                    return $q(function (resolve, reject) {
+
+                        var usersArr = $firebaseArray(new Firebase(users));
+                        var requests = [];
+
+                        usersArr.$loaded().then(function () {
+                            for (var i = 0; i < usersArr.length; i++) {
+                                var user = usersArr[i];
+                                if (user.requestCorporateSubmited) {
+                                    requests.push(user);
+                                }
+                            }
+                            resolve(requests);
+                        }).catch(function (error) {
+                            console.log(userid + 'does not exists');
+                            reject(error);
+                        });
+
+                    });
+                },
+
+            };
+        });
 })();
