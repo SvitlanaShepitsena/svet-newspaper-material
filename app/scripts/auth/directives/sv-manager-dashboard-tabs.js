303f8d4e9b136b29efb2730c6a7f9f2c92fd4820
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-25 12:05:25 -0500

    directive for users lists tabs on manager profile

--- app/scripts/auth/directives/sv-manager-dashboard-tabs.js
+++ app/scripts/auth/directives/sv-manager-dashboard-tabs.js
@@ -6,14 +6,21 @@
             return {
                 replace: true,
                 templateUrl: 'scripts/auth/directives/sv-manager-dashboard-tabs.html',
-                scope: {
-
-                },
-                link: function ($scope, el, attrs ) {
+                scope: {},
+                link: function ($scope, el, attrs) {
                     var tabs = [
-                            {title: 'my-profile', content: "scripts/auth/templates/manager-dashboard/manager-profile-temp.html"},
-                            {title: 'bookmarks', content: "scripts/auth/templates/manager-dashboard/manager-bookmarks-temp.html"},
-                            {title: 'profile-settings', content: "scripts/auth/templates/manager-dashboard/manager-profile-settings-temp.html"},
+                            {
+                                title: 'my-profile',
+                                content: "scripts/auth/templates/manager-dashboard/manager-profile-temp.html"
+                            },
+                            {
+                                title: 'bookmarks',
+                                content: "scripts/auth/templates/manager-dashboard/manager-bookmarks-temp.html"
+                            },
+                            {
+                                title: 'profile-settings',
+                                content: "scripts/auth/templates/manager-dashboard/manager-profile-settings-temp.html"
+                            },
                         ],
                         selected = null,
                         previous = null;
--- app/scripts/auth/directives/sv-manager-users-tabs.js
+++ app/scripts/auth/directives/sv-manager-users-tabs.js
@@ -6,10 +6,34 @@
             return {
                 replace: true,
                 templateUrl: 'scripts/auth/directives/sv-manager-users-tabs.html',
-                scope: {
-
-                },
+                scope: {},
                 link: function ($scope, el, attrs) {
+                    var tabs = [
+                            {
+                                title: 'all users',
+                                content: "scripts/auth/templates/manager-users/manager-all-users-temp.html"
+                            },
+                            {
+                                title: 'customers',
+                                content: "scripts/auth/templates/manager-users/manager-customers-temp.html"
+                            },
+                            {
+                                title: 'authors',
+                                content: "scripts/auth/templates/manager-users/manager-authors-temp.html"
+                            },
+                            {
+                                title: 'readers',
+                                content: "scripts/auth/templates/manager-users/manager-readers-temp.html"
+                            },
+                        ],
+                        selected = null,
+                        previous = null;
+                    $scope.tabs = tabs;
+                    $scope.selectedIndex = 0;
+                    $scope.$watch('selectedIndex', function (current, old) {
+                        previous = selected;
+                        selected = tabs[current];
+                    });
 
                 }
             };
