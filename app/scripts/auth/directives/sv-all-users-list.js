f5d63a26f5c2c05eb7288c39f77c9b488ac337ca
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-25 12:13:59 -0500

    moving functionality for displaying list of all users to  sv-all-users-list directive

--- app/scripts/auth/directives/sv-all-users-list.js
+++ app/scripts/auth/directives/sv-all-users-list.js
@@ -6,11 +6,24 @@
             return {
                 replace: true,
                 templateUrl: 'scripts/auth/directives/sv-all-users-list.html',
-                scope: {
+                scope: {},
+                controller: function ($scope, UserServ, UserGroupsServ, toastr) {
+                    var users = UserServ.all();
+                    $scope.users = users;
+
+                    users.$watch(function () {
+                        $scope.users = users;
+                    });
+
+                    $scope.changeGroup = function (user, group) {
+                        UserGroupsServ.toggleUserInGroup(user, group).then(function () {
+                            toastr.success('Group Membership has been successfully changed!')
+                        })
+
+                    };
 
                 },
                 link: function ($scope, el, attrs) {
-
                 }
             };
         });
