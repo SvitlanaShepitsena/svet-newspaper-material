5e8ca415a9f76843daa835cf1d8039a0bc0dd737
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-25 14:46:56 -0500

    Fix Customer Group Request bug

--- app/scripts/auth/controllers/CreateSvetEventCtrl.js
+++ app/scripts/auth/controllers/CreateSvetEventCtrl.js
@@ -3,15 +3,15 @@
 
 	angular.module('auth')
 		.controller('CreateSvetEventCtrl', function ($scope) {
-			$scope.event = {
-				title: '',
-				type: 'public',
-				address:'',
-				date:'',
-				admission:''
-		};
 
-});
+			$scope.event = {
+				type: 'public'
+			};
+
+
+
+
+		});
 })
 ();
 
