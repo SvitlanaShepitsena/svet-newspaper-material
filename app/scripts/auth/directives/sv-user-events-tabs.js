f1421740eb31d523de5cd3b7019e49a9f6565725
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-26 15:51:55 -0500

    Create Svet Connection Events

--- app/scripts/auth/directives/sv-user-events-tabs.js
+++ app/scripts/auth/directives/sv-user-events-tabs.js
@@ -2,26 +2,18 @@
     'use strict';
 
     angular.module('auth')
-        .directive('svUserEventsTabs', function () {
+        .directive('svUserEventsTabs', function (ConnectionEventServ) {
             return {
                 replace: true,
                 templateUrl: 'scripts/auth/directives/sv-user-events-tabs.html',
                 scope: {},
                 link: function ($scope, el, attrs) {
-                    var test = 'dddd';
-                    var tabs = [
-                            {title: 'planned-events', content: "Planned Events "},
-                            {title: 'visited-events', content: "Visited Events "},
-                            {title: 'invitations', content: test}
-                        ],
-                        selected = null,
-                        previous = null;
-                    $scope.tabs = tabs;
-                    $scope.selectedIndex = 0;
-                    $scope.$watch('selectedIndex', function (current, old) {
-                        previous = selected;
-                        selected = tabs[current];
-                    });
+                    $scope.events = ConnectionEventServ.allCorporate();
+                    $scope.data.selectedIndex = 2;
+
+                    $scope.acceptInvitation = function (svetEvent) {
+                        console.log(svetEvent);
+                    };
 
                 }
             };
