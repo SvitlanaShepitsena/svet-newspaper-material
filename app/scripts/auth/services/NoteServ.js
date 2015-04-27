03c0e2dc8726206bebc5e736ac0109893da3882c
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-27 09:52:54 -0500

    Notifications Functionality

--- app/scripts/auth/services/NoteServ.js
+++ app/scripts/auth/services/NoteServ.js
@@ -6,14 +6,18 @@
             function getCustomerNotifications(userId) {
 
                 return $q(function (resolve, reject) {
-                    var notifications = [];
                     var eventsArr = $firebaseArray(new Firebase(corporate));
                     eventsArr.$loaded().then(function () {
+                        var customerNotices = [];
                         eventsArr.forEach(function (event) {
-                            var customerIds = _.pluck(event.customers,'id');
-                            console.log(customerIds);
-                        })
-                    })
+                            var customerIds = _.pluck(event.customers, 'id');
+
+                            if (customerIds.indexOf(userId) === -1) {
+                                customerNotices.push(event);
+                            }
+                        });
+                        resolve(customerNotices);
+                    });
 
 
                 });
@@ -24,23 +28,32 @@
                 getNotifications: function (user) {
 
                     return $q(function (resolve, reject) {
-                        var notifications = [];
+                        var final = [];
 
                         var groups = user.groups;
                         groups.forEach(function (group) {
                             switch (group) {
                                 case 'customer':
                                     getCustomerNotifications(user.id).then(function (notices) {
-                                        notifications = _.union(notices);
+                                        if (notices.length) {
+                                            if (user.notifications) {
+                                                notices.forEach(function (notice) {
+                                                    user.notifications.push(notice);
+                                                });
+                                            } else{
+                                                user.notifications = notices;
+                                            }
+
+                                        }
                                     });
                                     break;
 
-                                default:
                             }
-                        })
-                        resolve(notifications);
+                        });
+
+                        resolve(user);
                     });
-                },
+                }
 
 
             };
