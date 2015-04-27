f23a0bcc48d4d8ebd8a845b1e1529b9a1aa0ccbb
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-27 14:06:43 -0500

    Number of new notifications icon design #225

--- app/scripts/notifications/directives/sv-notifications-list.js
+++ app/scripts/notifications/directives/sv-notifications-list.js
@@ -12,14 +12,17 @@
                         $scope.unopened = 0;
 
                     };
+                    $scope.showAll = function () {
+                        $scope.unopened = $scope.user.notifications.length;
+                    };
+
+
                     $rootScope.$watch('user', function (newVal) {
                         $scope.user = newVal;
                         if ($scope.user && $scope.user.notifications) {
 
                         $scope.unopened = _.where($scope.user.notifications, {opened: false}).length;
                         }
-
-
                     },true);
                     $rootScope.$watch('loadingUser', function (newVal) {
                         $scope.loadingUser = newVal;
