d5bfe1dca709d5db3d4b7159a86762bdf27ba2f4
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-04-27 16:28:06 -0500

    dir Attr Change

--- app/scripts/auth/directives/sv-unique-username.js
+++ app/scripts/auth/directives/sv-unique-username.js
@@ -2,11 +2,14 @@
     'use strict';
 
     angular.module('auth')
-        .directive('svUniqueUsername', function () {
+        .directive('svUniqueUsername', function (UserServ,$q,$timeout) {
             return {
                 require: '?^ngModel',
                 link: function ($scope, el, attrs, ctrl) {
+                    ctrl.$asyncValidators.username = function(modelValue, viewValue) {
 
+                        return UserServ.isUserNameUnique(modelValue);
+                    };
                 }
             };
         });
