ff678402c6c1c2a37072853538e94959ea840aa1
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-28 10:45:19 -0500

    Header Icons Design

--- app/scripts/notifications/directives/sv-notifications-list.js
+++ app/scripts/notifications/directives/sv-notifications-list.js
@@ -1,31 +1,26 @@
 (function () {
     'use strict';
-
     angular.module('notifications')
         .directive('svNotificationsList', function ($rootScope, $mdMedia) {
             return {
+                replace: true,
                 templateUrl: 'scripts/notifications/directives/sv-notifications-list.html',
                 scope: {},
                 link: function ($scope, el, attrs) {
-
-                    $scope.$watch(function() { return $mdMedia('sm'); }, function(sm) {
+                    $scope.$watch(function () {
+                        return $mdMedia('sm');
+                    }, function (sm) {
                         $scope.sm = sm;
                     });
-
-
                     $scope.markAllOpened = function () {
                         $scope.unopened = 0;
-
                     };
                     $scope.showAll = function () {
                         $scope.unopened = $scope.user.notifications.length;
                     };
-
-
                     $rootScope.$watch('user', function (newVal) {
                         $scope.user = newVal;
                         if ($scope.user && $scope.user.notifications) {
-
                             $scope.unopened = _.where($scope.user.notifications, {opened: false}).length;
                         }
                     }, true);
