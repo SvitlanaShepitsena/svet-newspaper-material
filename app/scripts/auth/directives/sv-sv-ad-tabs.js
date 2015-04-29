cbfd572f1031434014227e0db2c109dfc0db5f6f
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-29 11:39:28 -0500

    Add files

---
+++ app/scripts/auth/directives/sv-sv-ad-tabs.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('auth')
+        .directive('svSvAdTabs', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/auth/directives/sv-sv-ad-tabs.html',
+                scope: {
+
+                },
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
