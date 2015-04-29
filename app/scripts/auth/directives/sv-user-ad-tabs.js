cb0df663514d972827f5f5e0c914bc7f2a6c8c20
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-29 11:48:51 -0500

    Tabs navigation for Advertisement section

--- app/scripts/auth/directives/sv-user-ad-tabs.js
+++ app/scripts/auth/directives/sv-user-ad-tabs.js
@@ -1,16 +1,12 @@
 (function () {
     'use strict';
-
     angular.module('auth')
         .directive('svUserAdTabs', function () {
             return {
                 replace: true,
                 templateUrl: 'scripts/auth/directives/sv-user-ad-tabs.html',
-                scope: {
-
-                },
+                scope: {},
                 link: function ($scope, el, attrs) {
-
                 }
             };
         });
