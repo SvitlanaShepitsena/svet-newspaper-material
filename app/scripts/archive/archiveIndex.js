551c832cfb5166524014011587d1eb88d1267d62
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-29 13:10:48 -0500

    Archive routes separated (archiveIndex.js)

--- app/scripts/archive/archiveIndex.js
+++ app/scripts/archive/archiveIndex.js
@@ -1,6 +1,18 @@
 (function () {
     'use strict'
-
-    angular.module('archive', []);
+    angular.module('archive', ['ui.router'])
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                /*=archive*/
+                .state("app.new-light-newspaper", {
+                    url: "/new-light-newspaper",
+                    controller: "NewLightNewspaperCtrl as newLightNewspaper",
+                    templateUrl: "scripts/archive/views/new-light-newspaperCtrl.html"
+                })
+                .state("app.saturday-plus-newspaper", {
+                    url: "/saturday-plus-newspaper",
+                    controller: "SaturdayPlusNewspaperCtrl as saturdayPlusNewspaper",
+                    templateUrl: "scripts/archive/views/saturday-plus-newspaperCtrl.html"
+                })
+        });
 })();
-
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -61,17 +61,6 @@
                     templateUrl: "scripts/auth/views/svet-loginCtrl.html"
                 })
 
-                /*=archive*/
-                .state("app.new-light-newspaper", {
-                    url: "/new-light-newspaper",
-                    controller: "NewLightNewspaperCtrl as newLightNewspaper",
-                    templateUrl: "scripts/archive/views/new-light-newspaperCtrl.html"
-                })
-                .state("app.saturday-plus-newspaper", {
-                    url: "/saturday-plus-newspaper",
-                    controller: "SaturdayPlusNewspaperCtrl as saturdayPlusNewspaper",
-                    templateUrl: "scripts/archive/views/saturday-plus-newspaperCtrl.html"
-                })
                 .state("app.svet-recommends", {
                     url: "/svet-recommends",
                     controller: "SvetRecommendsCtrl as svetRecommends",
