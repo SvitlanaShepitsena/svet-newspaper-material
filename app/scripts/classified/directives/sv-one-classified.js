f6d2e08de8d9f097ad6aa7672c54af0ddb254ef5
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-04-29 15:52:24 -0500

    directive for displaying one classified

--- app/scripts/classified/directives/sv-one-classified.js
+++ app/scripts/classified/directives/sv-one-classified.js
@@ -1,16 +1,17 @@
 (function () {
     'use strict';
-
     angular.module('classified')
         .directive('svOneClassified', function () {
             return {
                 replace: true,
                 templateUrl: 'scripts/classified/directives/sv-one-classified.html',
                 scope: {
-
+                    cl: '=',
+                    removeCl: '&'
                 },
                 link: function ($scope, el, attrs) {
-
+                    var currentState = $state.$current.toString();
+                    $scope.isEditable = currentState.indexOf('app.user') > -1;
                 }
             };
         });
