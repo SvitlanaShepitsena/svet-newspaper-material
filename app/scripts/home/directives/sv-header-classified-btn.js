1587c109887a554dcbe7926801500005076df89e
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-01 12:15:41 -0500

    Header Classified Button

--- app/scripts/home/directives/sv-header-classified-btn.js
+++ app/scripts/home/directives/sv-header-classified-btn.js
@@ -1,16 +1,12 @@
 (function () {
     'use strict';
-
     angular.module('home')
         .directive('svHeaderClassifiedBtn', function () {
             return {
                 replace: true,
                 templateUrl: 'scripts/home/directives/sv-header-classified-btn.html',
-                scope: {
-
-                },
+                scope: {},
                 link: function ($scope, el, attrs) {
-
                 }
             };
         });
