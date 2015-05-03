61d408d64480342323dffd4b616c817305d219e4
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-05-03 09:59:17 -0500

    git bug

--- app/scripts/ad/directives/sv-home-ad.js
+++ app/scripts/ad/directives/sv-home-ad.js
@@ -2,14 +2,21 @@
     'use strict';
 
     angular.module('ad')
-        .directive('svHomeAd', function () {
+        .directive('svHomeAd', function (AdServ) {
             return {
                 replace: true,
                 templateUrl: 'scripts/ad/directives/sv-home-ad.html',
                 scope: {
-
+                    place: '@'
                 },
                 link: function ($scope, el, attrs) {
+                    var allAdds = AdServ.allArr();
+                    allAdds.$loaded().then(function (ads) {
+                        $scope.ad = _.chain(ads).where(function (ad) {
+                            return ad.place === $scope.place;
+                        }).shuffle().first().value();
+
+                    });
 
                 }
             };
