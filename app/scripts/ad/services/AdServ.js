ba95845a1241a482b8b0abe6b2678ef2ffa060e3
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-05-03 06:43:20 -0500

    Add files

---
+++ app/scripts/ad/services/AdServ.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    angular.module('ad')
+        .factory('AdServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+
+            return {
+
+                get: function () {
+
+                },
+
+                getAssync: function () {
+                    return $q(function (resolve, reject) {
+
+                    });
+                }
+            };
+        });
+})();
