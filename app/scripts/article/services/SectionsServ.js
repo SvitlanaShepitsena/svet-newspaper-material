8b468fb123819e072b306ea1ee49ae56884d2bbf
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-06 14:20:20 -0500

    Add files

---
+++ app/scripts/article/services/SectionsServ.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    angular.module('article')
+        .factory('SectionsServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+
+            return {
+
+                get: function () {
+
+                },
+
+                getAssync: function () {
+                    return $q(function (resolve, reject) {
+
+                    });
+                }
+            };
+        });
+})();
