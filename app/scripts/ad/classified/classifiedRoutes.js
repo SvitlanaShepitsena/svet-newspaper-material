d5cdb0341ed07f2e61d92f7c2cdf2c3fe1b044d3
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-06 13:12:09 -0500

    Displaying all classified bug #297

--- app/scripts/ad/classified/classifiedRoutes.js
+++ app/scripts/ad/classified/classifiedRoutes.js
@@ -8,48 +8,48 @@
                 .state("app.classified", {
                     url: "/classified",
                     controller: "ClassifiedCtrl as classified",
-                    templateUrl: "/views/classifiedCtrl.html"
+                    templateUrl: "scripts/ad/classified/views/classifiedCtrl.html"
                 })
                 .state("app.classified.all", {
                     url: "/all",
-                    templateUrl: "/views/all.html"
+                    templateUrl: "scripts/ad/classified/views/all.html"
                 })
                 .state("app.classified.community", {
                     url: "/community",
-                    templateUrl: "/views/classified-community-list.html"
+                    templateUrl: "scripts/ad/classified/views/classified-community-list.html"
                 })
                 .state("app.classified.job", {
                     url: "/job",
-                    templateUrl: "/views/classified-job-list.html"
+                    templateUrl: "scripts/ad/classified/views/classified-job-list.html"
                 })
                 .state("app.classified.lessons", {
                     url: "/lessons",
-                    templateUrl: "/views/classified-lessons-list.html"
+                    templateUrl: "scripts/ad/classified/views/classified-lessons-list.html"
                 })
                 .state("app.classified.housing", {
                     url: "/housing",
-                    templateUrl: "/views/classified-housing-list.html"
+                    templateUrl: "scripts/ad/classified/views/classified-housing-list.html"
                 })
                 .state("app.classified.sale", {
                     url: "/sale",
-                    templateUrl: "/views/classified-for-sale-list.html"
+                    templateUrl: "scripts/ad/classified/views/classified-for-sale-list.html"
                 })
                 .state("app.classified.services", {
                     url: "/services",
-                    templateUrl: "/views/classified-services-list.html"
+                    templateUrl: "scripts/ad/classified/views/classified-services-list.html"
                 })
                 .state("app.classified.personal", {
                     url: "/personal",
-                    templateUrl: "/views/classified-personal-list.html"
+                    templateUrl: "scripts/ad/classified/views/classified-personal-list.html"
                 })
                 .state("app.classified.cars", {
                     url: "/cars",
-                    templateUrl: "/views/classified-cars-list.html"
+                    templateUrl: "scripts/ad/classified/views/classified-cars-list.html"
                 })
                 .state("app.classified.one-classified", {
                     url: "/:clid",
                     controller: "OneClassifiedCtrl as oneClassified",
-                    templateUrl: "/views/one-classifiedCtrl.html"
+                    templateUrl: "scripts/ad/classified/views/one-classifiedCtrl.html"
                 })
 //#state'
         });
--- app/scripts/ad/classified/directives/sv-header-classified-btn.js
+++ app/scripts/ad/classified/directives/sv-header-classified-btn.js
@@ -1,10 +1,10 @@
 (function () {
     'use strict';
-    angular.module('sections.header')
+    angular.module('ad.classified')
         .directive('svHeaderClassifiedBtn', function () {
             return {
                 replace: true,
-                templateUrl: 'scripts/sections/header/directives/sv-header-classified-btn.html',
+                templateUrl: 'scripts/ad/classified/directives/sv-header-classified-btn.html',
                 scope: {},
                 link: function ($scope, el, attrs) {
                 }
