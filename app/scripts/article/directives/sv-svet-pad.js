2af81ee254f6192c520fc49ad86cdb2a031fec1d
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-05-07 14:32:37 -0500

    FirePad

--- app/scripts/article/directives/sv-svet-pad.js
+++ app/scripts/article/directives/sv-svet-pad.js
@@ -2,14 +2,22 @@
     'use strict';
 
     angular.module('article')
-        .directive('svSvetPad', function () {
+        .directive('svSvetPad', function (url) {
+
             return {
                 replace: true,
                 templateUrl: 'scripts/article/directives/sv-svet-pad.html',
                 scope: {
-
+                    body: '='
                 },
                 link: function ($scope, el, attrs) {
+                    var firepadRef = new Firebase(url+'firepad');
+                    var content = el.find('#firepad')[0];
+
+                    var codeMirror = CodeMirror(content, { lineWrapping: true });
+                    var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
+                        { richTextShortcuts: true, richTextToolbar: true, defaultText: ''});
+
 
                 }
             };
