90988cd6d9cd9faa48a6c9e3b67ee5b287e8e877
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-08 10:47:44 -0500

    sextAngular directive

--- app/scripts/article/directives/sv-svet-pad.js
+++ app/scripts/article/directives/sv-svet-pad.js
@@ -11,15 +11,8 @@
                 link: function ($scope, el, attrs) {
                     var btns = el.find('button').css('padding', '6px 12px');
                     btns.css('float', 'left');
-                    var btngroup = el.find('btn-group').css('display', 'block');
-                    btngroup.css('display', 'block');
-                    btngroup.css('float', 'left');
-                    btngroup.css('font-size', '14px');
-                    btngroup.css('line-height', '20px');
-                    btngroup.css('margin-bottom', '5px');
-                    btngroup.css('margin-left', '5px');
-                    btngroup.css('position', 'relative');
-                    btngroup.css('vertical-align', 'middle');
+                    btns.css('font-size', '12px');
+                    btns.css('color', '#757575');
                     console.log('text');
                     $scope.myContent = '';
                 }
