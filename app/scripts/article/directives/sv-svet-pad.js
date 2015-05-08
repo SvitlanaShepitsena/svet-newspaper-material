4a665757ff687a2a09e23b7dd53f1a877e109a00
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-08 09:40:14 -0500

    sextAngular directive

--- app/scripts/article/directives/sv-svet-pad.js
+++ app/scripts/article/directives/sv-svet-pad.js
@@ -9,6 +9,17 @@
                     body: '='
                 },
                 link: function ($scope, el, attrs) {
+                    var btns = el.find('button').css('padding', '6px 12px');
+                    btns.css('float', 'left');
+                    var btngroup = el.find('btn-group').css('display', 'block');
+                    btngroup.css('display', 'block');
+                    btngroup.css('float', 'left');
+                    btngroup.css('font-size', '14px');
+                    btngroup.css('line-height', '20px');
+                    btngroup.css('margin-bottom', '5px');
+                    btngroup.css('margin-left', '5px');
+                    btngroup.css('position', 'relative');
+                    btngroup.css('vertical-align', 'middle');
                     console.log('text');
                     $scope.myContent = '';
                 }
