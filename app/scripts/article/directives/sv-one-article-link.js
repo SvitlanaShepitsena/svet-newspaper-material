6fc2d0a49f76d4c8ab08600bb67986d986ed1bda
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-08 12:33:33 -0500

    Add files

--- app/scripts/article/directives/sv-one-article-link.js
+++ app/scripts/article/directives/sv-one-article-link.js
@@ -5,7 +5,7 @@
         .directive('svOneArticleLink', function () {
             return {
                 replace: true,
-                templateUrl: 'scripts/aritcle/directives/sv-one-article-link.html',
+                templateUrl: 'scripts/article/directives/sv-one-article-link.html',
                 scope: {
 
                 },
