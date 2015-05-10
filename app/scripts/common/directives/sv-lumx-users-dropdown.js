bf5c04ba18e7f530c4a4619a093f37e0236aef9f
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-10 15:05:09 -0500

    Universal directive for lumx dropdown

--- app/scripts/common/directives/sv-lumx-users-dropdown.js
+++ app/scripts/common/directives/sv-lumx-users-dropdown.js
@@ -1,16 +1,19 @@
 (function () {
     'use strict';
-
     angular.module('common')
         .directive('svLumxUsersDropdown', function () {
             return {
                 replace: true,
                 templateUrl: 'scripts/common/directives/sv-lumx-users-dropdown.html',
                 scope: {
-
+                    btnTitle: '@',
+                    actionCounter: '@',
+                    userAvatar: '@',
+                    userName: '@',
+                    userFirstName: '@',
+                    userLastName: '@'
                 },
                 link: function ($scope, el, attrs) {
-
                 }
             };
         });
