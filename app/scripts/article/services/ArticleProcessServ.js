72de473efae71cb5385cf762f9cb1e8e2a7ce5cb
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-05-10 15:04:04 -0500

    Add files

---
+++ app/scripts/article/services/ArticleProcessServ.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    angular.module('article')
+        .factory('ArticleProcessServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+
+            return {
+
+                get: function () {
+
+                },
+
+                getAssync: function () {
+                    return $q(function (resolve, reject) {
+
+                    });
+                }
+            };
+        });
+})();
