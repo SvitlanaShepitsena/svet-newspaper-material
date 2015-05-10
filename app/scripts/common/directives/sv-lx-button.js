87d531dfc36c977bd032a6104c4228b99ce65d18
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-10 12:59:59 -0500

    Universal Directives for manage an article buttons #311

--- app/scripts/common/directives/sv-lx-button.js
+++ app/scripts/common/directives/sv-lx-button.js
@@ -1,16 +1,23 @@
 (function () {
     'use strict';
-
     angular.module('common')
         .directive('svLxButton', function () {
             return {
                 replace: true,
                 templateUrl: 'scripts/common/directives/sv-lx-button.html',
                 scope: {
-
+                    url: '@',
+                    methodClick: '&',
+                    btnSize: '@',
+                    btnType: '@',
+                    btnColor: '@',
+                    textColor: '@',
+                    bgColor: '@',
+                    btnTitle: '@',
+                    iconType: '@',
+                    iconName: '@'
                 },
                 link: function ($scope, el, attrs) {
-
                 }
             };
         });
