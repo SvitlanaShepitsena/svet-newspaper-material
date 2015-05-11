1ededdfdd4279edb52e55cc847fab696ec8e1ca6
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-05-11 09:22:07 -0500

    Fix Logout Previous User Data CleanUp Problems

--- app/scripts/article/directives/sv-article-status.js
+++ app/scripts/article/directives/sv-article-status.js
@@ -3,10 +3,10 @@
     angular.module('article')
         .directive('svArticleStatus', function () {
             return {
-                replace: true,
                 templateUrl: 'scripts/article/directives/sv-article-status.html',
                 scope: {},
                 link: function ($scope, el, attrs) {
+                    console.log('run here sv-article-status.js');
                 }
             };
         });
