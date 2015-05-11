ff2b4082edf4c15cbf50b6e299ed2513ae15aedf
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-11 09:23:47 -0500

    compiling directives jade

--- app/scripts/article/directives/sv-article-status.js
+++ app/scripts/article/directives/sv-article-status.js
@@ -7,6 +7,7 @@
                 templateUrl: 'scripts/article/directives/sv-article-status.html',
                 scope: {},
                 link: function ($scope, el, attrs) {
+                    console.log('run here sv-article-status.js');
                 }
             };
         });
