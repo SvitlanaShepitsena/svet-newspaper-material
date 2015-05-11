df31251a6dcd529fb8f7bce2eeb22f80f612ccdf
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-11 11:25:24 -0500

    directive for displaying article statistics works

--- app/scripts/article/directives/sv-aritcle-public-statistics.js
+++ app/scripts/article/directives/sv-aritcle-public-statistics.js
@@ -1,16 +1,12 @@
 (function () {
     'use strict';
-
     angular.module('article')
-        .directive('svAritcleStatistics', function () {
+        .directive('svAritclePublicStatistics', function () {
             return {
                 replace: true,
-                templateUrl: 'scripts/article/directives/sv-aritcle-statistics.html',
-                scope: {
-
-                },
+                templateUrl: 'scripts/article/directives/sv-aritcle-public-statistics.html',
+                scope: {},
                 link: function ($scope, el, attrs) {
-
                 }
             };
         });
