8df6f7b608ea512b92a41c857ca281b34825c4dc
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-16 11:40:13 -0500

    Add files

---
+++ app/scripts/sections/home/directives/sv-classified-header-phone.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('sections.home')
+        .directive('svClassifiedHeaderPhone', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/sections/home/directives/sv-classified-header-phone.html',
+                scope: {
+
+                },
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
