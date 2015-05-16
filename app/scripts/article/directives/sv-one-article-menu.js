7705cbf867900e90216f051179a03686902ad183
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-16 14:14:47 -0500

    Add files

---
+++ app/scripts/article/directives/sv-one-article-menu.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('article')
+        .directive('svOneArticleMenu', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/article/directives/sv-one-article-menu.html',
+                scope: {
+
+                },
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
