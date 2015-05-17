d28267671421da2b0659aa3a90e260f35456415e
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-05-16 19:52:51 -0500

    Add files

---
+++ app/scripts/auth/notifications/services/TestMeToServ.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+    angular.module('auth.notifications')
+        .factory('TestMeToServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+            return {
+                get: function () {
+                },
+                getAssync: function () {
+                    return $q(function (resolve, reject) {
+                    });
+                }
+            };
+        });
+})();
