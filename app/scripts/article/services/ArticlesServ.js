cc8381c893038d89646705a2b33b438afb1cbc4f
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-18 15:02:00 -0500

    Add files

--- app/scripts/article/services/ArticlesServ.js
+++ app/scripts/article/services/ArticlesServ.js
@@ -1,12 +1,19 @@
 (function () {
     'use strict';
     angular.module('article')
-        .factory('ArticlesServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+        .factory('ArticlesServ', function (NewsTimeSelectorServ, $q, url, users, $firebaseObject, $firebaseArray) {
+            var ref = new Firebase(url + 'articles');
+            var articlesArr = $firebaseArray(ref);
+
+            function getPublicNews(articles) {
+                var publicNews = _.where(articles, {isPublic: true, isBlog: false});
+            }
+
             return {
-                get: function () {
-                },
-                getAssync: function () {
+                setHomeNewsLive: function () {
                     return $q(function (resolve, reject) {
+                        articlesArr.$loaded(function () {
+                        })
                     });
                 }
             };
