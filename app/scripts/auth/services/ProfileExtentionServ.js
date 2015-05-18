0e007cab1636ecfaca4f9ee8395fd8e4c125780a
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-18 09:27:43 -0500

    Add files

---
+++ app/scripts/auth/services/ProfileExtentionServ.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+    angular.module('auth')
+        .factory('ProfileExtentionServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+            return {
+                get: function () {
+                },
+                getAssync: function () {
+                    return $q(function (resolve, reject) {
+                    });
+                }
+            };
+        });
+})();
