6abeef048e50733f68457ee1bd83991eb424da53
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-20 11:43:37 -0500

    Add files

---
+++ app/scripts/ad/promotion/directives/sv-bunner11.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('ad.promotion')
+        .directive('svBunner11', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/ad/promotion/directives/sv-bunner-1-1.html',
+                scope: {
+
+                },
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
