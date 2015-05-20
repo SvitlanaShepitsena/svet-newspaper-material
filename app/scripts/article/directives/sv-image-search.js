4743ec1213b8ec16013ee5d6bfdcd34ac98a7e44
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-05-20 09:52:56 -0500

    Add files

--- app/scripts/article/directives/sv-image-search.js
+++ app/scripts/article/directives/sv-image-search.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('article')
-        .directive('svImageSearch', function () {
+        .directive('svImageSearch', function (ImageSearchServ, $http) {
             return {
                 replace: true,
                 templateUrl: 'scripts/article/directives/sv-image-search.html',
@@ -10,7 +10,14 @@
 
                 },
                 link: function ($scope, el, attrs) {
+                    $scope.runGoogleSearch = function () {
+                        var url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=maya+plisetskaya&callback=JSON_CALLBACK";
 
+                        $http.jsonp(url)
+                            .success(function(data){
+                                console.log(data);
+                            });
+                    };
                 }
             };
         });
