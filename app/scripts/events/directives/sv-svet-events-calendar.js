26592af00077c6c1521cd576d7b1982cc0f3ca50
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-05-20 15:30:19 -0500

    Lodash

--- app/scripts/events/directives/sv-svet-events-calendar.js
+++ app/scripts/events/directives/sv-svet-events-calendar.js
@@ -4,13 +4,8 @@
     angular.module('events')
         .directive('svSvetEventsCalendar', function () {
             return {
-                replace: true,
                 templateUrl: 'scripts/events/directives/sv-svet-events-calendar.html',
-                scope: {
-
-                },
                 link: function ($scope, el, attrs) {
-
                 }
             };
         });
