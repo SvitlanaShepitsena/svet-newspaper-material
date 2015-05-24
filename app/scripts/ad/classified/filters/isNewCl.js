d5541738a35c61bc5064aff3c7be8c00a3fb1414
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-05-24 09:17:54 -0500

    Slider animation WORKS!!!

--- app/scripts/ad/classified/filters/isNewCl.js
+++ app/scripts/ad/classified/filters/isNewCl.js
@@ -1,12 +1,18 @@
 (function () {
     'use strict';
-
     angular.module('ad.classified')
         .filter('isNewCl', function () {
-            return function (list) {
-                return _.where(list, function (item) {
-                    return item;
-                });
+            return function (ts) {
+                if (!ts) {
+                    return;
+                }
+                var now = parseInt(moment().format('x'));
+                var parsed = parseInt(ts);
+
+                var format=(now-parsed)/(3600);
+                if (format < 24) {
+                    return 'New!'
+                }
             };
         });
 })();
