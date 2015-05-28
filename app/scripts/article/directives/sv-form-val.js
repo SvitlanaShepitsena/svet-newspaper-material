90c166484df9bca60fb825a7626f724fc1e93af8
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-27 22:11:14 -0500

    Dynamic form validation using directive whit $compile service and ng-messages-include

--- app/scripts/article/directives/sv-form-val.js
+++ app/scripts/article/directives/sv-form-val.js
@@ -3,31 +3,41 @@
     angular.module('article')
         .directive('svFormVal', function ($compile) {
             return {
+                restrict: 'A',
+                /*Require another directive and inject its controller as the fourth argument to the linking function
+                 */
                 require: '?^form',
-                compile: function (elTemp) {
-                    return function ($scope, el, attrs, ctrl) {
+                compile: function (elTemp, attr) {
+                    return function ($scope, el, attr, ctrl) {
                         var formName = ctrl.$name;
+                        /*find required form elements with JQuery*/
                         var inputs = el.find('input[required=""]');
                         var txtareas = el.find('textarea[required=""]');
-
+                        /*combine two arrays in one with Lodash method*/
                         var formElements = _.union(inputs, txtareas);
+                        /*attached compiled message to each element of array*/
                         angular.forEach(formElements, function (formElement) {
-                            attachMessages(formElement,formElement.name);
+                            attachMessages(formElement, formElement.name);
                         });
-
-                        var radioGroup = el.find('md-radio-group');
-                        var radioGroupsName = radioGroup.attr('name');
-                        attachMessages(radioGroup,radioGroupsName);
-
-
+                        var radioGroup = el.find('md-radio-group[required=""]');
+                        /*get element name with JQuery*/
+                        var radioGroupName = radioGroup.attr('name');
+                        attachMessages(radioGroup, radioGroupName);
+                        /**/
                         function attachMessages(formElement, elementName) {
-                            var fullFormElement = formName + '.' + elementName;
-                            var fullFormElementError = fullFormElement + '.$error';
-                            var messages = "<div ng-if='" + fullFormElement + ".$touched || " + formName + ".$submitted' ng-messages='" + fullFormElementError + "'>" +
-                                "<div ng-messages-include='scripts/common/templates/form-error-messages.html'></div>" +
-                                "</div>";
-                            var messagesCompilled = $compile(messages)($scope);
-                            angular.element(formElement).after(messagesCompilled);
+                            var appFormElement = formName + '.' + elementName;
+                            var appFormElementTouched = appFormElement + '.$touched';
+                            var appFormElementError = appFormElement + '.$error';
+                            var appFormSubmitted = formName + '.$submitted';
+                            /*create validation message*/
+                            var message = "<div ng-if='" + appFormElementTouched + "|| " +
+                                appFormSubmitted + "'ng-messages='" + appFormElementError + "'>" +
+                                "<div ng-messages-include='scripts/common/templates/form-error-messages.html'>" +
+                                "</div>" + "</div>";
+                            /**/
+                            var messagesCompiled = $compile(message)($scope);
+                            /*after each required form element message will be compiled*/
+                            angular.element(formElement).after(messagesCompiled);
                         }
                     }
                 }
--- app/scripts/events/directives/sv-new-afisha-event-form.js
+++ app/scripts/events/directives/sv-new-afisha-event-form.js
@@ -9,13 +9,13 @@
                     if (!$scope.editState) {
                         // Scaffold Data
                         $scope.event = {
-                            title: "New Event #" + _.random(0, 100),
+                            //title: "New Event #" + _.random(0, 100),
                             startsAt: moment($scope.clickedDate).add(10, 'h').toDate(),
                             endsAt: moment($scope.clickedDate).add(13, 'h').toDate(),
                             address: faker.address.streetAddress() + " Chicago, IL, 60634",
                             location: 'Copernicus Center',
                             locationWeb: faker.internet.domainName(),
-                            about: "http://" + faker.lorem.paragraph() + '.com',
+                            //about: "http://" + faker.lorem.paragraph() + '.com',
                             tickets: 'http://www.ticketmaster.com/'
                         };
                         $scope.event.date = $scope.clickedDate;
@@ -41,7 +41,7 @@
                     $scope.createAfishaEvent = function (formValid) {
                         if (formValid) {
                             ConnectionEventServ.savePublicEvent($scope.event).then(function () {
-                                toastr.info('Thank you for event addition.');
+                                toastr.info('Thank you for creating an event.');
                                 $scope.hide();
                             });
                         }
