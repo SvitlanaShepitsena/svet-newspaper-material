5ddbe8a024255fb248480e82a8f88f3be8eb6f8f
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-29 16:09:02 -0500

    Events Confirmation with Directive

--- app/scripts/events/directives/sv-one-event.js
+++ app/scripts/events/directives/sv-one-event.js
@@ -1,16 +1,18 @@
 (function () {
     'use strict';
-
     angular.module('events')
         .directive('svOneEvent', function () {
             return {
                 replace: true,
                 templateUrl: 'scripts/events/directives/sv-one-event.html',
                 scope: {
-
+                    event: '=',
+                    hide: '&dialogHide'
                 },
                 link: function ($scope, el, attrs) {
-
+                    $scope.isDialog = function () {
+                        return attrs.dialogHide;
+                    };
                 }
             };
         });
