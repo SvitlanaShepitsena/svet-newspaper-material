8d3c9a96ea8e070eac92172237aa1b367281d66c
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-05-30 17:34:53 -0500

    Event thumbnail design and functionality #420

--- app/scripts/events/filters/previosFutureFr.js
+++ app/scripts/events/filters/previosFutureFr.js
@@ -3,9 +3,15 @@
 
     angular.module('events')
         .filter('previosFutureFr', function () {
-            return function (list) {
-                return _.where(list, function (item) {
-                    return item;
+            return function (list,isFuture) {
+
+                return _.filter(list, function (item) {
+                    if (isFuture) {
+                    return moment().isBefore(item.startsAt);
+                    } else{
+
+                    return moment().isAfter(item.startsAt);
+                    }
                 });
             };
         });
