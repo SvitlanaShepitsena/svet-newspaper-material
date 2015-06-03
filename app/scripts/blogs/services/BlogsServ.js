fe5c7842461f7fde7c23b42f1dde84a09b983417
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-03 14:37:46 -0500

    Add files

---
+++ app/scripts/blogs/services/BlogsServ.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+    angular.module('blogs')
+        .factory('BlogsServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+            return {
+                get: function () {
+                },
+                getAssync: function () {
+                    return $q(function (resolve, reject) {
+                    });
+                }
+            };
+        });
+})();
