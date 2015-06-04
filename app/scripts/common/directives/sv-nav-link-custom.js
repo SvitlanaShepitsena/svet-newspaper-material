393256b6803aa0dc2bc5910b54d68f284c7d7c88
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-04 15:15:17 -0500

    Directive for navigation link with a custom icon #452

--- app/scripts/common/directives/sv-nav-link-custom.js
+++ app/scripts/common/directives/sv-nav-link-custom.js
@@ -2,14 +2,27 @@
     'use strict';
 
     angular.module('common')
-        .directive('svNavLinkCustom', function () {
+        .directive('svNavLinkCustom', function ($mdSidenav) {
             return {
                 replace: true,
                 templateUrl: 'scripts/common/directives/sv-nav-link-custom.html',
                 scope: {
+                    url: '@',
+                    iconWidth: '@',
+                    iconLink: '@',
+                    stitle: '@',
+                    side: '@',
+                    icon: '@',
+                    someObject: '=',
+                    someString: '@',
+                    someExpr: '&'
 
                 },
                 link: function ($scope, el, attrs) {
+                    $scope.closeSideBar = function () {
+                        $mdSidenav('left').close();
+                        $mdSidenav('right').close();
+                    };
 
                 }
             };
