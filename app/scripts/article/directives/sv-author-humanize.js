197b77bce722cf4759bc860232252b1bbd86f460
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-06-05 16:36:59 -0500

    Quotes

--- app/scripts/article/directives/sv-author-humanize.js
+++ app/scripts/article/directives/sv-author-humanize.js
@@ -7,6 +7,16 @@
                 require: '?^ngModel',
                 link: function ($scope, el, attrs, ctrl) {
 
+                    ctrl.$formatters.push(function (modelValue) {
+                        if (!modelValue) {
+                            return;
+                        }
+
+                        var hum = (_.map(modelValue.split('-'), function (initial) {
+                            return _.capitalize(initial);
+                        })).join(' ');
+                        return hum;
+                    });
                 }
             };
         });
