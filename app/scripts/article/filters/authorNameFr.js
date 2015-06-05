73ee079bb711c72eb48f8b38123239251e8417b3
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-05 12:37:20 -0500

    Displaying author name bug

--- app/scripts/article/filters/authorNameFr.js
+++ app/scripts/article/filters/authorNameFr.js
@@ -3,11 +3,14 @@
 
     angular.module('article')
         .filter('authorNameFr', function () {
-                return function (userName) {
-                    return (_.map(userName.split('-'), function (initial) {
-                        return _.capitalize(initial);
-                    })).join(' ');
+            return function (userName) {
+                if (!userName || !_.isString(userName)) {
+                    return;
+                }
+                return (_.map(userName.split('-'), function (initial) {
+                    return _.capitalize(initial);
+                })).join(' ');
 
-                };
+            };
         });
 })();
