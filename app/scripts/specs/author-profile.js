3e6f206d3080b53da987e2e1a64b33ca11c15290
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-06-07 11:49:31 -0500

    Svet Several Article Save Procedure #472

--- app/scripts/specs/author-profile.js
+++ app/scripts/specs/author-profile.js
@@ -55,30 +55,30 @@ describe('editor profile functionality', function () {
 
 
 
-    //it('It should not Save Article twice by clicking save btn sequencially', function () {
-    //    element(by.id('addNews')).click();
-    //    element(by.tagName('md-select')).click();
-    //    element(by.id('1')).click();
-    //
-    //    element(by.model('article.topic')).sendKeys('Test topic');
-    //    element(by.model('article.title')).sendKeys('Test title ' + Math.random().toFixed(2));
-    //    element(by.model('article.summary')).sendKeys('Test summary');
-    //    element(by.model('html')).sendKeys('Test body');
-    //    element(by.model('article.tags')).sendKeys('Test tags');
-    //
-    //    element(by.id('submitArticle')).click().then(function () {
-    //        element(by.id('submitArticle')).click().then(function () {
-    //            element(by.id('linkBack')).click().then(function () {
-    //                element.all(by.repeater('article in articles')).then(function (articles) {
-    //                    var titleElement = articles[0].element(by.id('authorArticleLinkTitle'));
-    //                    var titleElement2 = articles[1].element(by.id('authorArticleLinkTitle'));
-    //                    expect(titleElement.getText()).not.toEqual(titleElement2.getText());
-    //                });
-    //            });
-    //
-    //        });
-    //    });
-    //});
+    it('It should not Save Article twice by clicking save btn sequencially', function () {
+        element(by.id('addNews')).click();
+        element(by.tagName('md-select')).click();
+        element(by.id('1')).click();
+
+        element(by.model('article.topic')).sendKeys('Test topic');
+        element(by.model('article.title')).sendKeys('Test title ' + Math.random().toFixed(2));
+        element(by.model('article.summary')).sendKeys('Test summary');
+        element(by.model('html')).sendKeys('Test body');
+        element(by.model('article.tags')).sendKeys('Test tags');
+
+        element(by.id('submitArticle')).click().then(function () {
+            element(by.id('submitArticle')).click().then(function () {
+                element(by.id('linkBack')).click().then(function () {
+                    element.all(by.repeater('article in articles')).then(function (articles) {
+                        var titleElement = articles[0].element(by.id('authorArticleLinkTitle'));
+                        var titleElement2 = articles[1].element(by.id('authorArticleLinkTitle'));
+                        expect(titleElement.getText()).not.toEqual(titleElement2.getText());
+                    });
+                });
+
+            });
+        });
+    });
 
     afterEach(function () {
         //browser.get('http://localhost:3000/#/alexander-etman/articles').then(function () {
