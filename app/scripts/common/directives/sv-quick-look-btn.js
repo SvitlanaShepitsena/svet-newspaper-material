5f770d2fe3818c558e56f60ec2a2c86b2166698d
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-07 13:13:40 -0500

    Add files

---
+++ app/scripts/common/directives/sv-quick-look-btn.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('common')
+        .directive('svQuickLookBtn', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/common/directives/sv-quick-look-btn.html',
+                scope: {
+
+                },
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
