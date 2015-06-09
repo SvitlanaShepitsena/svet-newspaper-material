9422fcc19eb1d2a573ba716043dcd017ab33c61e
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-06-09 11:14:37 -0500

    Image Upload Works

--- app/scripts/article/directives/sv-image-upload.js
+++ app/scripts/article/directives/sv-image-upload.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     angular.module('article')
-        .directive('svImageUpload', function () {
+        .directive('svImageUpload', function ($rootScope, taSelection) {
             return {
                 templateUrl: 'scripts/article/directives/sv-image-upload.html',
                 link: function ($scope, el, attrs) {
@@ -20,15 +20,22 @@
                             fileReader.readAsDataURL(file.file);
                             fileReader.onload = function (event) {
                                 if ($scope.article) {
-                                    $scope.article.img = event.target.result;
+                                    //$scope.article.img = event.target.result;
+                                    runMe(event.target.result);
+
                                 }
                                 if ($scope.event) {
                                     $scope.event.img = event.target.result;
                                 }
                             };
                         });
+                    };
+
+                    function runMe(selectedImage) {
+                        var selection = $rootScope.lastSelection;
+                        var embed = '<img src=' + selectedImage + ' contenteditable="true" allowfullscreen="true" frameborder="0" ng-style="padding:5px" />';
+                        taSelection.insertHtml(embed, undefined, selection);
                     }
-                    ;
                 }
             };
         });
