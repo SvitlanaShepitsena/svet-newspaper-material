5bf6bfd6015b4e9a08796726e6a648333bdff5d7
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-10 22:46:31 -0500

    Sidenav Layout bugs on Mozilla #503

--- app/scripts/sections/home/directives/sv-classified-header-phone.js
+++ app/scripts/sections/home/directives/sv-classified-header-phone.js
@@ -1,16 +1,12 @@
 (function () {
     'use strict';
-
     angular.module('sections.home')
         .directive('svClassifiedHeaderPhone', function () {
             return {
                 replace: true,
                 templateUrl: 'scripts/sections/home/directives/sv-classified-header-phone.html',
-                scope: {
-
-                },
+                scope: {},
                 link: function ($scope, el, attrs) {
-
                 }
             };
         });
