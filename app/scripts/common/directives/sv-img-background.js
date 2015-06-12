d14b2fd15aa2bdd530c48eeb017278a0259d9276
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-12 14:04:37 -0500

    Image

--- app/scripts/common/directives/sv-img-background.js
+++ app/scripts/common/directives/sv-img-background.js
@@ -7,9 +7,28 @@
                 replace: true,
                 templateUrl: 'scripts/common/directives/sv-img-background.html',
                 scope: {
-
+                    svImage: '=',
+                    linkRoute: '@',
                 },
                 link: function ($scope, el, attrs) {
+
+                    var img = new Image();
+                    img.src = $scope.image;
+                    img.onload = function () {
+                        var width = img.width;
+                        var height = img.height;
+
+                        var breakPoint = 1;
+
+                        var iElm = el.find('i');
+                        iElm.css({
+                            'background-image': 'url(' + $scope.svImage + ')',
+                            'background-size': 'cover',
+                             'z-index':10,
+                             'width':'100%',
+
+                        });
+                    }
 
                 }
             };
