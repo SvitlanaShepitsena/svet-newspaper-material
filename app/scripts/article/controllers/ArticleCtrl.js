d9784472f7e6fdeeefaa660fd353ec20b9e2025b
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-06-12 14:54:52 -0500

    Clean Article

--- app/scripts/article/controllers/ArticleCtrl.js
+++ app/scripts/article/controllers/ArticleCtrl.js
@@ -1,13 +1,12 @@
 (function () {
     'use strict';
     angular.module('article')
-        .controller('ArticleCtrl', function ($scope, ArticlesServ, $stateParams, userAuth) {
-            console.log('run here ArticleCtrl.js');
+        .controller('ArticleCtrl', function ($scope, ArticlesServ, $stateParams, userAuth, SvHtmlValidatorServ) {
             $scope.user = userAuth.profile;
             $scope.userKey = userAuth.key;
             var id = $stateParams.id;
             ArticlesServ.get(id).$loaded().then(function (article) {
-                $scope.article = article;
+                $scope.article = SvHtmlValidatorServ.cleanArticle(article);
             });
         });
 })();
