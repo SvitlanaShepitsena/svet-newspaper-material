979de7e3d88ce5d3b9eb600d340c3bf67c570d02
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-06-13 18:29:16 -0500

    Home Tags

--- app/scripts/article/filters/tagsFr.js
+++ app/scripts/article/filters/tagsFr.js
@@ -10,11 +10,18 @@
                 var artWithTags = _.filter(list, function (article) {
                     var tags = article.tags;
                     if (tags) {
-                        var arrTags= _.map(tags.split(','), function (oneTag) {
-                         return oneTag.toLowerCase().trim();
+                        tags = tags.trim().toLowerCase();
+                        if (_.contains(tags, ',')) {
+                            var collection = tags.split(',');
+
+                        } else {
+                            collection = tags.split(' ');
+                        }
+                        var arrTags = _.map(collection, function (oneTag) {
+                            return oneTag.toLowerCase().trim();
                         });
-                    var inter=_.intersection(arrTags, filterTags);
-                    return inter.length>0;
+                        var inter = _.intersection(arrTags, filterTags);
+                        return inter.length > 0;
                     }
                 });
 
