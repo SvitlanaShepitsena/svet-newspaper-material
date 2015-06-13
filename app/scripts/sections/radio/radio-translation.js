f740fccae784e577bbccf2b52b31a3020ea5d771
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-12 23:40:48 -0500

    Home page radio directive design #516

--- app/scripts/sections/radio/radio-translation.js
+++ app/scripts/sections/radio/radio-translation.js
@@ -7,13 +7,31 @@
             $translateProvider.useCookieStorage();
 
             $translateProvider.translations('en', {
-               // 'eng':'eng'
-               // 'eng':'eng',
+                'radio': 'Radio',
+                'radio-subhead-1': ' Radio Talk show',
+                'radio-subhead-2': ' With Alex Etman',
+                'radio-body-1': 'Every Sunday ',
+                'radio-body-2': 'on 1240AM',
+                'radio-body-3': '11:00 AM - 1:00 PM.',
+                'radio-archive': 'Radio OSA Archive',
+                'radio-files-archive': 'Archive',
+                'back': 'Back',
+                // 'eng':'eng'
+                // 'eng':'eng',
             });
 
             $translateProvider.translations('ru', {
-               // 'ru':'ru'
-               // 'ru':'ru',
+                'radio': 'Радио',
+                'radio-subhead-1': 'Радио Tок-шоу',
+                'radio-subhead-2': 'с Александром Этманом',
+                'radio-body-1': 'Каждое воскресенье',
+                'radio-body-2': 'на 1240AM',
+                'radio-body-3': '11:00 - 13:00 ',
+                'radio-archive': 'Архивные передачи',
+                'radio-files-archive': 'Архив передач',
+                'back': 'Вернуться',
+                // 'ru':'ru'
+                // 'ru':'ru',
             });
 
             $translateProvider.preferredLanguage('en');
