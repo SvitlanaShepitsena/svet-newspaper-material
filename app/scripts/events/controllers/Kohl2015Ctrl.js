2654f3aa45196c3991446ac130aa39ac41f970c3
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-06-14 17:15:07 -0500

    git bug

--- app/scripts/events/controllers/Kohl2015Ctrl.js
+++ app/scripts/events/controllers/Kohl2015Ctrl.js
@@ -3,6 +3,11 @@
 
     angular.module('events')
         .controller('Kohl2015Ctrl', function ($scope) {
+            $scope.images=[
+                {thumb: '/img/gallery/1th.jpg', img: 'img/gallery/1.jpg'},
+                {thumb: '/img/gallery/2th.jpg', img: 'img/gallery/2.jpg'},
+                {thumb: '/img/gallery/3th.jpg', img: 'img/gallery/3.jpg'}
+            ]
 
         });
 })();
