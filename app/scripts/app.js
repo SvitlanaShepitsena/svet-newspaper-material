3ef7cf0f4d878870ac4d789c7e43a8c36fd199c0
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-06-14 16:34:26 -0500

    Fix top article title

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,6 +6,7 @@
         'pascalprecht.translate',
         'underscore.string',
         'auth',
+        'jkuri.gallery',
         'ad',
         'ngMaterial',
         'ngDragDrop',
@@ -59,18 +60,18 @@
             /*radio programs*/
         })
         //// COMMENT ON PRODUCTION
-        .factory('$exceptionHandler', function ($injector) {
-            return function (exception, cause) {
-                var $rootScope = $injector.get('$rootScope');
-                var toastr = $injector.get('toastr');
-                exception.message = exception.stack;
-
-                //Comment on Production
-                toastr.error('ERROR!' + exception.message);
-                $rootScope.$broadcast('error');
-                throw exception;
-            };
-        })
+        //.factory('$exceptionHandler', function ($injector) {
+        //    return function (exception, cause) {
+        //        var $rootScope = $injector.get('$rootScope');
+        //        var toastr = $injector.get('toastr');
+        //        exception.message = exception.stack;
+        //
+        //        //Comment on Production
+        //        toastr.error('ERROR!' + exception.message);
+        //        $rootScope.$broadcast('error');
+        //        throw exception;
+        //    };
+        //})
         .config(['$compileProvider', function ($compileProvider) {
             $compileProvider.debugInfoEnabled(false);
         }]);
