5f1a027c0fd1a7b1a29ba3862bfc4c3c5b5f670f
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-06-18 09:30:07 -0500

    Fb share/ like btn

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -46,7 +46,7 @@
     ])
         .config(function (ezfbProvider) {
             ezfbProvider.setInitParams({
-                appId: '258826184311868',
+                appId: '1405000443143632',
                 version: 'v2.3'
             });
         })
