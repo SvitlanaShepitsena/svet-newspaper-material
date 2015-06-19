7ece3eeada1573f9fce35e2374bf8be90e779601
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-19 15:33:59 -0500

    Add files

---
+++ app/scripts/sections/about/directives/sv-top-ru-speaking.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('sections.about')
+        .directive('svTopRuSpeaking', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/sections/about/directives/sv-top-ru-speaking.html',
+                scope: {
+
+                },
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
