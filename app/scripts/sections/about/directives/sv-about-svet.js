9c440efcc78c060cea2d9d3e85efc9fb2c094604
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-19 16:15:30 -0500

    Add files

---
+++ app/scripts/sections/about/directives/sv-about-svet.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('sections.about')
+        .directive('svAboutSvet', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/sections/about/directives/sv-about-svet.html',
+                scope: {
+
+                },
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
