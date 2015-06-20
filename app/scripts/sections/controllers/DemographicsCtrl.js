f01ba6f7f91e669e5fc97f1c268be3cdd8f91f12
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-20 17:43:10 -0500

    Add files

--- app/scripts/sections/controllers/DemographicsCtrl.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('sections')
-        .controller('DemographicsCtrl', function ($scope) {
-
-        });
-})();
-
--- app/scripts/sections/sectionsRoutes.js
+++ app/scripts/sections/sectionsRoutes.js
@@ -5,13 +5,8 @@
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
 
-				.state("app.demographics", {
-					url: "/demographics", 
-					controller:"DemographicsCtrl",
-					templateUrl: "scripts/sections/views/demographicsCtrl.html"
-				})
+				
 //#state'
 
     });
 })();
-
