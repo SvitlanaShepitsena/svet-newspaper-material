b359796039df0013af8ea817e084b2319b9098d0
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-06-21 11:38:01 -0500

    Custom Avatar

--- app/scripts/ad/promotion/directives/sv-customer-avatar.js
+++ app/scripts/ad/promotion/directives/sv-customer-avatar.js
@@ -2,15 +2,16 @@
     'use strict';
 
     angular.module('ad.promotion')
-        .directive('svCustomerAvatar', function () {
+        .directive('svCustomerAvatar', function (avatar) {
             return {
                 replace: true,
                 templateUrl: 'scripts/ad/promotion/directives/sv-customer-avatar.html',
                 scope: {
+                    customer:'='
 
                 },
                 link: function ($scope, el, attrs) {
-
+                    $scope.defaultAvatar=avatar;
                 }
             };
         });
