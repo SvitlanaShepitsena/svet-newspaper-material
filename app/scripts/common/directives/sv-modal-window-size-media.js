e90e5bad5f7c40333cc2dd4670e43cb1a01c8a4f
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-25 14:59:17 -0500

    Dynamic size for google demographic map with decorating directive #599

--- app/scripts/common/directives/sv-modal-window-size-media.js
+++ app/scripts/common/directives/sv-modal-window-size-media.js
@@ -2,11 +2,27 @@
     'use strict';
 
     angular.module('common')
-        .directive('svModalWindowSizeMedia', function () {
+        .directive('svModalWindowSizeMedia', function ($mdMedia) {
             return {
-                require: '?^ngModel',
                 link: function ($scope, el, attrs, ctrl) {
+                    return {
+                        link: function ($scope, el, attrs) {
+                            console.log('run here sv-modal-window-size-media.js');
+                            $scope.$watch(function () {
+                                return $mdMedia('sm');
+                            }, function (sm) {
+                                if (sm) {
 
+                                    el.css('min-height', '100%');
+                                    el.css('min-width', '100%');
+                                }
+                                else {
+                                    el.css('height', 'auto');
+                                    el.css('width', 'auto');
+                                }
+                            });
+                        }
+                    };
                 }
             };
         });
