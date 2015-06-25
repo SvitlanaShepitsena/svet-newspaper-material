e4c497e96d574cb812cec0a22267aa4016eee8d3
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-06-25 11:04:08 -0500

    Add edit comment functionality #596

--- app/scripts/article/directives/sv-one-comment.js
+++ app/scripts/article/directives/sv-one-comment.js
@@ -2,15 +2,27 @@
     'use strict';
 
     angular.module('article')
-        .directive('svOneComment', function (userAuth) {
+        .directive('svOneComment', function (userAuth, CommentsServ) {
             return {
                 replace: true,
                 templateUrl: 'scripts/article/directives/sv-one-comment.html',
                 scope: {
+                    articleKey: '@',
                     comment: '=',
                     removeComment: '&'
                 },
                 link: function ($scope, el, attrs) {
+                    $scope.editState = false;
+
+                    $scope.saveCommentChanges = function () {
+
+                        CommentsServ.updateComment($scope.articleKey, $scope.comment).then(function (id) {
+
+                            $scope.editState = false;
+                        });
+                    };
+
+
                     $scope.isEditable = function () {
                         if (!userAuth.profile) {
                             return false;
