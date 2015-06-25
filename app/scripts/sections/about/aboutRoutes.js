8a9ebe847d307fbe3905f3a57e20bcdbdf91b808
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-25 09:54:26 -0500

    Add files

--- app/scripts/sections/about/aboutRoutes.js
+++ app/scripts/sections/about/aboutRoutes.js
@@ -1,3 +1,8 @@
+				.state("app.about-tab-content", {
+					url: "/about-tab-content", 
+					controller:"AboutTabContentCtrl",
+					templateUrl: "scripts/sections/about/views/about-tab-contentCtrl.html"
+				})
 (function () {
     'use strict'
 
@@ -7,11 +12,18 @@
 
                 .state("app.about", {
                     url: "/about",
+                    abstract: true,
                     controller: "AboutCtrl",
                     templateUrl: "scripts/sections/about/views/aboutCtrl.html"
                 })
-//#state'
-
+                .state("app.about.svet", {
+                    url: "/about-svet-russian-media-group",
+                    templateUrl: "scripts/sections/about/views/about-tab-content.html"
+                })
+                .state("app.about.demographics", {
+                    url: "/demographics",
+                    templateUrl: "scripts/sections/about/views/demographicsTabsContent.html"
+                })
         });
 })();
 
