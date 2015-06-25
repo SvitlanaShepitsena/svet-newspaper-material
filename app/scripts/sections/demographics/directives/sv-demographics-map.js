6ee48a4b0e7dd7bb4c77eb61f0f1ae875b41c68d
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-25 00:58:26 -0500

    Add files

---
+++ app/scripts/sections/demographics/directives/sv-demographics-map.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('sections.demographics')
+        .directive('svDemographicsMap', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/sections/demographics/directives/sv-demographics-map.html',
+                scope: {
+
+                },
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
