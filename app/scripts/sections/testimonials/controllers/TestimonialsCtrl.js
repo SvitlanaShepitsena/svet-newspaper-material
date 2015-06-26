8482ea6d91ae8f531b80d26871a84ef93a8506fe
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-26 13:55:59 -0500

    Modal Window for Feedback Form #605

--- app/scripts/sections/testimonials/controllers/TestimonialsCtrl.js
+++ app/scripts/sections/testimonials/controllers/TestimonialsCtrl.js
@@ -2,8 +2,27 @@
     'use strict';
 
     angular.module('sections.testimonials')
-        .controller('TestimonialsCtrl', function ($scope) {
+        .controller('TestimonialsCtrl', function ($scope, $mdDialog) {
 
+            $scope.showFeedbackModal = function () {
+                $mdDialog.show(
+                    {
+                        controller: FeedbackModalController,
+                        templateUrl: 'scripts/sections/testimonials/views/modalFeedback.html',
+                    }
+                );
+            };
+            function FeedbackModalController($scope, $mdDialog) {
+                $scope.hide = function () {
+                    $mdDialog.hide();
+                };
+                $scope.cancel = function () {
+                    $mdDialog.cancel();
+                };
+                $scope.answer = function (answer) {
+                    $mdDialog.hide(answer);
+                };
+            }
         });
 })();
 
