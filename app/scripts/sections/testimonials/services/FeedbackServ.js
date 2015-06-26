fa9ff395e01e78088c86a70ae6620084e2825a8e
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-06-26 12:31:53 -0500

    Add Feedback Form Design and Functionality #604i

--- app/scripts/sections/testimonials/services/FeedbackServ.js
+++ app/scripts/sections/testimonials/services/FeedbackServ.js
@@ -2,11 +2,16 @@
     'use strict';
     angular.module('sections.testimonials')
         .factory('FeedbackServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+            var ref = new Firebase(url + 'feedbacks');
             return {
-                get: function () {
-                },
-                getAssync: function () {
+                saveFeedback: function (feedback) {
                     return $q(function (resolve, reject) {
+                        var feedbacksArr = $firebaseArray(ref);
+                        feedbacksArr.$add(feedback).then(function (ref) {
+                            resolve(ref.key());
+                        }).catch(function (error) {
+                            reject(error);
+                        });
                     });
                 }
             };
