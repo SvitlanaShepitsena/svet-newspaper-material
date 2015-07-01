8b04c996e31cc74ef380f5b57e230f0799da1422
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-07-01 14:16:34 -0500

    Add files

---
+++ app/scripts/common/directives/sv-img-framed-stringng.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('common')
+        .directive('svImgFramedStringng', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/common/directives/sv-img-framed-stringng.html',
+                scope: {
+
+                },
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();
