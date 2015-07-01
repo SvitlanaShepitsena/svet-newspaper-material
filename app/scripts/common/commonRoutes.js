f88d0452345a993841a9b46752054154885fcd10
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-07-01 13:52:08 -0500

    Add files

--- app/scripts/common/commonRoutes.js
+++ app/scripts/common/commonRoutes.js
@@ -13,6 +13,11 @@
                     controller: "GaCtrl",
                     templateUrl: "scripts/common/views/gaCtrl.html"
                 })
+				.state("app.one-photo-gallery", {
+					url: "/one-photo-gallery", 
+					controller:"One-photo-galleryCtrl",
+					templateUrl: "scripts/common/views/one-photo-galleryCtrl.html"
+				})
 //#state'
         });
 })();
