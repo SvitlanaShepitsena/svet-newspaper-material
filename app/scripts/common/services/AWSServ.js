86205e7f1256abb903100395e41bb8c7765c26fb
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-07-03 11:58:15 -0500

    Add files

---
+++ app/scripts/common/services/AWSServ.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+    angular.module('common')
+        .factory('AWSServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+            return {
+                get: function () {
+                },
+                getAssync: function () {
+                    return $q(function (resolve, reject) {
+                    });
+                }
+            };
+        });
+})();
